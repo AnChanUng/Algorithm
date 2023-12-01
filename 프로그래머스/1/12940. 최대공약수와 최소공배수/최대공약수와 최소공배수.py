380e799ba971fa04990ff46fc1e53a4d30d6129a
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2023-12-01 21:29:56 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,8 +1,8 @@
-def solution(a, b):
-    c, d = max(a, b), min(a, b)
-    t = 1
-    while t > 0:
-        t = c % d
-        c, d = d, t
-    answer = [c, int(a*b/c)]
+def solution(n, m):
+    answer = []
+    
+    for i in range(n
+        answer.appned(n)
+    
+    print(answer)
     return answer
