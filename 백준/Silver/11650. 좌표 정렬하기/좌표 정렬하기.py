7cfab65dc7cca295e44a34321ac64aa424a918b0
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-03-16 01:40:30 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4092 ms, Memory: 44580 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,8 +1,10 @@
-N = int(input())
+n = int(input())
 arr = []
-for i in range(N):
+for _ in range(n):
     a, b = map(int, input().split())
     arr.append((a, b))
+
 arr.sort()
-for i in range(N):
+
+for i in range(n):
     print(arr[i][0], arr[i][1])
