3b16cc8e008fcae9006ff6fb7409be7b856ad27b
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-02 01:38:28 +0900

    [Silver V] Title: 피보나치 수 4, Time: 84 ms, Memory: 35044 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,13 @@
+n = int(input())
+array = []
+for i in range(n+1):
+    array.append(i)
+
+dp = [0] * 10001
+
+dp[0] = 0
+dp[1] = 1
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
