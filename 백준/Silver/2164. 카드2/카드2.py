e6529ec62b8cd482838f7437cc93f8b68a840755
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-04 19:10:01 +0900

    [Silver IV] Title: 카드2, Time: 220 ms, Memory: 50928 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+queue = deque()
+for i in range(1, n+1):
+    queue.append(i)
+
+while len(queue) != 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue[0])
