d685eb572ece0832c2e6935b4b397f0078bfa0db
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-08 03:43:00 +0900

    [Bronze I] Title: 더하기 사이클, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 26
+num = n # 26
+count = 0
+
+while True:
+    a = num // 10 #2
+    b = num % 10 #6
+    c = (a+b) % 10 #8
+    num = (b*10)+c #68
+
+    count += 1
+    if (num == n):
+        break
+print(count)
