7b758aca838f73e69efc277b8da2a1740c6203a1
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-08 19:53:34 +0900

    [Silver IV] Title: 다음 소수, Time: 1456 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for _ in range(t):
+    n = int(input())
+    while not is_prime(n):
+        n += 1
+    print(n)
