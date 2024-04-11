e88df67153b962655792f683dbfc71c962794297
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-11 13:14:45 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -1,17 +1,16 @@
 n = int(input())
-cnt = 0
-li = []
-
+array = []
 for _ in range(n):
-    cow, road = map(int, input().split())
-    li.append([cow, road])
+    cow_number, witch = map(int, input().split())
+    array.append([cow_number, witch])
 
+cnt = 0
 for i in range(n):
-    for j in range(i+1,n):
-        if li[i][0] == li[j][0]:
-            if abs(li[i][1]- li[j][1]) == 0:
+    for j in range(i+1, n):
+        if array[i][0] == array[j][0]:
+            if abs(array[i][1]-array[j][1]) == 0:
                 break
-            if abs(li[i][1] - li[j][1]) == 1:
+            if abs(array[i][1]-array[j][1]) == 1:
                 cnt += 1
                 break
 
