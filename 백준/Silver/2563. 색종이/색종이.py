8af9001918cb80f1488faeb8d89059f8cbd8f26c
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-21 15:12:53 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,15 @@
+n = int(input())
+array = [[0 for _ in range(101)] for _ in range(101)]
+
+for _ in range(n):
+    x, y = list(map(int, input().split()))
+
+    for row in range(x, x+10):
+        for col in range(y, y+10):
+            array[row][col] = 1
+
+result = 0
+
+for i in array:
+    result += i.count(1)
+print(result)
