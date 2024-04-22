b3b68bc16531c8aed88760e47fb748149d65a595
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-04-22 14:54:27 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,16 +1,17 @@
-exp = input().split('-')
+sik = input().split("-")
 
 num = []
-for i in exp:
+
+for i in sik:
     sum = 0
     tmp = i.split('+')
     for j in tmp:
         sum += int(j)
     num.append(sum)
 
-n = num[0]  # 식의 제일 처음은 숫자로 시작하기 때문에 0번째 값에서 나머지 값들을 빼준다.
+n = num[0]
 
 for i in range(1, len(num)):
-    n = n - num[i]
+    n -= num[i]
 
 print(n)
