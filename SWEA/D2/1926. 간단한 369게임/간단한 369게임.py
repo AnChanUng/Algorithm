e0dfbc2eb40dcc93e0dd2af30772dbf1b70a28b9
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-05-02 23:47:14 +0900

    [D2] Title: 간단한 369게임, Time: 100 ms, Memory: 43,188 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -0,0 +1,13 @@
+n = int(input())
+array = []
+for i in range(1, n+1):
+    cnt = 0
+    for j in range(len(str(i))):
+        if str(i)[j] == '3' or str(i)[j] == '6' or str(i)[j] == '9':
+            cnt += 1
+    if cnt > 0:
+        array.append(cnt * '-')
+    else:
+        array.append(i)
+
+print(*array)
