7924b7b59bdf24bc7b21b54bd9d312f55e2fbe47
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-05-06 00:25:46 +0900

    [D3] Title: 숫자 조작, Time: 146 ms, Memory: 48,540 KB -BaekjoonHub

---
+++ SWEA/D3/13428. 숫자 조작/숫자 조작.py
@@ -0,0 +1,21 @@
+def swap(i, j, string):
+    s_list = list(string)
+    s_list[i], s_list[j] = s_list[j], s_list[i]
+    number = ''.join(s_list)
+    return number
+
+t = int(input())
+for test_case in range(1, t+1):
+    num = input()
+    result_min = int(num)
+    result_max = int(num)
+    for i in range(len(num)-1):
+        for j in range(i, len(num)):
+            number = swap(i, j, num)
+            if number[0] == '0':
+                continue
+            if int(number) > result_max:
+                result_max = int(number)
+            if int(number) < result_min:
+                result_min = int(number)
+    print(f'#{test_case}', result_min, result_max)
