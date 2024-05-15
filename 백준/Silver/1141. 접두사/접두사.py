3153f625f40d1efb65292b53b2960f30dc9f26ec
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-05-15 16:08:02 +0900

    [Silver I] Title: 접두사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.py
@@ -0,0 +1,19 @@
+n = int(input())
+words = []
+for _ in range(n):
+    words.append(input())
+
+words.sort(key=len)
+res = 0
+
+for i in range(n):
+    flag = False
+    for j in range(i+1, n):
+        if words[i] == words[j][0:len(words[i])]:
+            flag = True
+            break
+    
+    if not flag:
+        res += 1
+
+print(res)
