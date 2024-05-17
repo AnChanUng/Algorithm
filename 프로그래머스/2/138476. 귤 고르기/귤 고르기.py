823d4ddcfd6b7eff115dc41b9149dc25d0687ce0
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-05-17 21:22:56 +0900

    [level 2] Title: 귤 고르기, Time: 58.44 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,12 +1,20 @@
-from collections import Counter
 def solution(k, tangerine):
-    # 귤 크기별 수량에 따른 정렬
-    sort_list = sorted(Counter(tangerine).items(), key=lambda pair: pair[1], reverse=True)
+    answer = 0
+    dict = {}
+    tangerine.sort()
     
-    # '종류 수'와 '종류 별 수량' for loop
-    for i, t in enumerate(sort_list, start=1):
-        # 수량이 많은 귤부터 담기
-        k -= t[1]
-        # 상자가 가득차면 담은 횟수(인덱스) 리턴
-        if(k <= 0):
-            return i
+    for i in tangerine:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    
+    d = sorted(dict.items(), key = lambda x: x[1], reverse=True)
+    
+    for i in d:
+        k = k - i[1]
+        answer += 1
+        if (k <= 0):
+            break
+            
+    return answer
