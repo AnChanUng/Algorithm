037264a502da3ebc9b34f433e1ef12da16c22992
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-05-26 20:37:45 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,13 +1,8 @@
 def solution(n):
     
-    yak_su = []
-    cnt = 0
-    
+    total = 0
     for i in range(1, n+1):
         if n % i == 0:
-            yak_su.append(i)
+            total += i
     
-    for j in range(len(yak_su)):
-        cnt += yak_su[j]
-    
-    return cnt
+    return total
