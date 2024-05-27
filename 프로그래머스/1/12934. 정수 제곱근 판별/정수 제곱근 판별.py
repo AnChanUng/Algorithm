a6b8025aa3be5e9e5c88c8358976d47d0cdd560f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-05-27 14:42:07 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,11 +1,6 @@
 def solution(n):
     
-    #n의 제고급 (루트 씌우기)
-    n = n ** (0.5)
-    
-    #n이 정수면 (n+1)**2 리턴
-    if n % 1 == 0:
-        return (n+1)**2
-    
-    return -1
-    
+    if n % (n ** 0.5) == 0:
+        return (n**0.5+1)**2
+    else:
+        return -1
