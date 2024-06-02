2ebad9435af46c99d01d6c91508dbae3fc0f64bc
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-06-02 18:00:34 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,8 +1,6 @@
 def solution(s):
-    
-    middle = len(s) // 2
-    
+
     if len(s) % 2 == 0:
-        return s[middle-1: middle+1]
+        return s[len(s)//2-1:len(s)//2+1]
     else:
-        return s[middle]
+        return s[len(s)//2]
