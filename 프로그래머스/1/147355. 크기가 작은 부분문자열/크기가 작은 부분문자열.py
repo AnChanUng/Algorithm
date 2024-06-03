d1d490fd31413d7ce3146535a250bef79c374d85
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-06-03 14:59:46 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,9 +1,9 @@
 def solution(t, p):
-    answer = 0
-    p_len = len(p)
-    t_len = len(t)
-    p = int(p)
-    for i in range(0, t_len+1-p_len):
-        if int(t[i:i+p_len]) <= p:
-            answer += 1
-    return answer
+    
+    t = list(t)
+    cnt = 0
+    for i in range(len(t)-len(p)+1):
+        if ''.join(t[i:i+len(p)]) <= p:
+            cnt += 1
+
+    return cnt
