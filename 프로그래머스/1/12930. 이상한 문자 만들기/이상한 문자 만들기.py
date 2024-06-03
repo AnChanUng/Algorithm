506d9596e1e8cfb01ced196a4e4802cb5cb3e234
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-06-03 16:55:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,12 +1,14 @@
 def solution(s):
-    answer = []
-
-    for voca in s.split(" "):
-        word = ""
-        for i in range(len(voca)):
-            if i % 2 == 1:
-                word += voca[i].lower()
+    words = s.split(" ")
+    
+    modified_words = []
+    for word in words:
+        modified_word = ""
+        for i in range(len(word)):
+            if i % 2 == 0:
+                modified_word += word[i].upper()
             else:
-                word += voca[i].upper()
-        answer.append(word)
-    return ' '.join(answer)
+                modified_word += word[i].lower()
+        modified_words.append(modified_word)
+    
+    return " ".join(modified_words)
