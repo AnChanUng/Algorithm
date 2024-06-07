f73be1f87c96a1456feb95fb35f4eb0ac22cd5a6
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-06-07 23:10:14 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -0,0 +1,182 @@
+def solution(a, b):
+    
+    if a == 1:
+        if b % 7 == 1:
+            return "FRI"
+        elif b % 7 == 2:
+            return "SAT"
+        elif b % 7 == 3:
+            return "SUN"
+        elif b % 7 == 4:
+            return "MON"
+        elif b % 7 == 5:
+            return "TUE"
+        elif b % 7 == 6:
+            return "WED"
+        elif b % 7 == 0:
+            return "THU"
+    elif a == 2:
+        if (31 + b) % 7 == 4:
+            return "MON"
+        elif (31 + b) % 7 == 5:
+            return "TUE"
+        elif (31 + b) % 7 == 6:
+            return "WED"
+        elif (31 + b) % 7 == 0:
+            return "THU"
+        elif (31 + b) % 7 == 1:
+            return "FRI"
+        elif (31 + b) % 7 == 2:
+            return "SAT"
+        elif (31 + b) % 7 == 3:
+            return "SUN"
+    elif a == 3:
+        if (60 + b) % 7 == 4:
+            return "MON"
+        elif (60 + b) % 7 == 5:
+            return "TUE"
+        elif (60 + b) % 7 == 6:
+            return "WED"
+        elif (60 + b) % 7 == 0:
+            return "THU"
+        elif (60 + b) % 7 == 1:
+            return "FRI"
+        elif (60 + b) % 7 == 2:
+            return "SAT"
+        elif (60 + b) % 7 == 3:
+            return "SUN"
+    elif a == 4:
+        if (91 + b) % 7 == 4:
+            return "MON"
+        elif (91 + b) % 7 == 5:
+            return "TUE"
+        elif (91 + b) % 7 == 6:
+            return "WED"
+        elif (91 + b) % 7 == 0:
+            return "THU"
+        elif (91 + b) % 7 == 1:
+            return "FRI"
+        elif (91 + b) % 7 == 2:
+            return "SAT"
+        elif (91 + b) % 7 == 3:
+            return "SUN"
+    elif a == 5:
+        if (121 + b) % 7 == 4:
+            return "MON"
+        elif (121 + b) % 7 == 5:
+            return "TUE"
+        elif (121 + b) % 7 == 6:
+            return "WED"
+        elif (121 + b) % 7 == 0:
+            return "THU"
+        elif (121 + b) % 7 == 1:
+            return "FRI"
+        elif (121 + b) % 7 == 2:
+            return "SAT"
+        elif (121 + b) % 7 == 3:
+            return "SUN"
+    elif a == 6:
+        if (152 + b) % 7 == 4:
+            return "MON"
+        elif (152 + b) % 7 == 5:
+            return "TUE"
+        elif (152 + b) % 7 == 6:
+            return "WED"
+        elif (152 + b) % 7 == 0:
+            return "THU"
+        elif (152 + b) % 7 == 1:
+            return "FRI"
+        elif (152 + b) % 7 == 2:
+            return "SAT"
+        elif (152 + b) % 7 == 3:
+            return "SUN"
+    elif a == 7:
+        if (182 + b) % 7 == 4:
+            return "MON"
+        elif (182 + b) % 7 == 5:
+            return "TUE"
+        elif (182 + b) % 7 == 6:
+            return "WED"
+        elif (182 + b) % 7 == 0:
+            return "THU"
+        elif (182 + b) % 7 == 1:
+            return "FRI"
+        elif (182 + b) % 7 == 2:
+            return "SAT"
+        elif (182 + b) % 7 == 3:
+            return "SUN"
+    elif a == 8:
+        if (213 + b) % 7 == 4:
+            return "MON"
+        elif (213 + b) % 7 == 5:
+            return "TUE"
+        elif (213 + b) % 7 == 6:
+            return "WED"
+        elif (213 + b) % 7 == 0:
+            return "THU"
+        elif (213 + b) % 7 == 1:
+            return "FRI"
+        elif (213 + b) % 7 == 2:
+            return "SAT"
+        elif (213 + b) % 7 == 3:
+            return "SUN"
+    elif a == 9:
+        if (244 + b) % 7 == 4:
+            return "MON"
+        elif (244 + b) % 7 == 5:
+            return "TUE"
+        elif (244 + b) % 7 == 6:
+            return "WED"
+        elif (244 + b) % 7 == 0:
+            return "THU"
+        elif (244 + b) % 7 == 1:
+            return "FRI"
+        elif (244 + b) % 7 == 2:
+            return "SAT"
+        elif (244 + b) % 7 == 3:
+            return "SUN"
+    elif a == 10:
+        if (274 + b) % 7 == 4:
+            return "MON"
+        elif (274 + b) % 7 == 5:
+            return "TUE"
+        elif (274 + b) % 7 == 6:
+            return "WED"
+        elif (274 + b) % 7 == 0:
+            return "THU"
+        elif (274 + b) % 7 == 1:
+            return "FRI"
+        elif (274 + b) % 7 == 2:
+            return "SAT"
+        elif (274 + b) % 7 == 3:
+            return "SUN"
+    elif a == 11:
+        if (305 + b) % 7 == 4:
+            return "MON"
+        elif (305 + b) % 7 == 5:
+            return "TUE"
+        elif (305 + b) % 7 == 6:
+            return "WED"
+        elif (305 + b) % 7 == 0:
+            return "THU"
+        elif (305 + b) % 7 == 1:
+            return "FRI"
+        elif (305 + b) % 7 == 2:
+            return "SAT"
+        elif (305 + b) % 7 == 3:
+            return "SUN"
+    elif a == 12:
+        if (335 + b) % 7 == 4:
+            return "MON"
+        elif (335 + b) % 7 == 5:
+            return "TUE"
+        elif (335 + b) % 7 == 6:
+            return "WED"
+        elif (335 + b) % 7 == 0:
+            return "THU"
+        elif (335 + b) % 7 == 1:
+            return "FRI"
+        elif (335 + b) % 7 == 2:
+            return "SAT"
+        elif (335 + b) % 7 == 3:
+            return "SUN"
