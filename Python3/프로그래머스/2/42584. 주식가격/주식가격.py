7bb6c586e9a17be801b1632e4ce31cf8c9ee7f58
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-08-07 17:09:05 +0900

    [level 2] Title: 주식가격, Time: 149.46 ms, Memory: 19.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/42584. 주식가격/주식가격.py
+++ Python3/프로그래머스/2/42584. 주식가격/주식가격.py
@@ -3,10 +3,11 @@ def solution(prices):
     for i in range(len(prices)):
         cnt = 0
         for j in range(i+1, len(prices)):
-            if prices[i] > prices[j]:
+            if prices[i] <= prices[j]:
+                cnt += 1
+            else:
                 cnt += 1
                 break
-            cnt += 1
             
         array.append(cnt)
     
