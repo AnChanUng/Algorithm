775655df379324db9da733a58bc0c4b9af6b2077
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-08-14 10:59:12 +0900

    [Silver V] Title: 영화감독 숌, Time: 604 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+cnt = 0
+num = 666
+while True:
+    if '666' in str(num):
+        cnt += 1
+        
+    if cnt == n:
+        break
+
+    num += 1
+
+print(num)
