d74540580291c7bba1ea46c8f15f15b3594b506e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-08-20 11:34:12 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+answer = []
+visited = [0] * n
+
+def backtracking(start):
+    
+    if len(answer) == m:
+        print(*answer)
+        return
+    
+    remember = 0
+    for i in range(n):
+        if not visited[i] and remember != numbers[i]:
+            if i >= start:
+                visited[i] = 1
+                answer.append(numbers[i])
+                remember = numbers[i]
+                backtracking(i)
+                answer.pop()
+                visited[i] = 0
+                
+backtracking(0)
