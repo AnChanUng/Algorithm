f33f992b2e1e27988d0e31bb02bcb16b4ed8e512
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-17 20:57:49 +0900

    [Gold V] Title: 빗물, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Gold/14719. 빗물/빗물.py
+++ Python/백준/Gold/14719. 빗물/빗물.py
@@ -1,12 +1,13 @@
 h, w = map(int, input().split())
 block = list(map(int, input().split()))
-answer = 0
 
-for i in range(1, w-1):
+answer = 0
+for i in range(1, len(block)-1):
     left = max(block[:i])
     right = max(block[i+1:])
-    m = min(left, right)
-    if m > block[i]:
-        answer += m - block[i]
+    
+    min_block = min(left, right)
+    if min_block > block[i]:
+        answer += min_block - block[i]
 
 print(answer)
