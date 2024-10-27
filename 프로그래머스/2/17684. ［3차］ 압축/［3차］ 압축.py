8aba687c9b1a30520751e0e2c41d2ec8b87ad677
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-27 14:32:12 +0900

    [level 2] Title: [3차] 압축, Time: 0.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,10 +1,8 @@
 def solution(msg):
     answer = []
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     dic = {}
     for i in range(1, 27):
         dic[chr(i+64)] = i
-    
     w = 0
     c = 0
     while True:
@@ -12,7 +10,7 @@ def solution(msg):
         if c == len(msg):
             answer.append((dic[msg[w:c]]))
             break
-        
+            
         if msg[w:c+1] not in dic:
             dic[msg[w:c+1]] = len(dic) + 1
             answer.append(dic[msg[w:c]])
