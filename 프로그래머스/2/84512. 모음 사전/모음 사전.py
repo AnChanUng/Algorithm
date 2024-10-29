b7ba41b25428ef59569208aa8f045fdfe4764a1b
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-29 19:46:03 +0900

    [level 2] Title: 모음 사전, Time: 8.34 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -3,8 +3,10 @@ from itertools import product
 def solution(word):
     
     words = []
+    moeum = ['A', 'E', 'I', 'O', 'U']
     for i in range(1, 6):
-        for j in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+        for j in product(moeum, repeat=i):
             words.append(''.join(list(j)))
+            print(j)
     words.sort()
     return words.index(word)+1
