883fecbdd3a0d16c1245f4a28ab56de1f27a60e5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-29 09:48:20 +0900

    [level 2] Title: 소수 찾기, Time: 2324.26 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+def sosu(n):
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def solution(numbers):
+    cnt = 0
+    arr = set()
+    for i in range(1, len(numbers)+1):
+        for j in permutations(numbers, i):
+            result = ''.join(j)
+            arr.add(int(result))
+        
+    for i in arr:
+        if sosu(i):
+            cnt += 1
+    
+    return cnt
