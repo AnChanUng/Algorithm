86145dddbf5c893575aecb4f39196eb315beb45f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-31 16:15:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 61.97 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,32 @@
+def is_valid(s):
+    stack = []
+    for i in s:
+        if i in '[{(':
+           stack.append(i)
+        
+        elif i == ')':
+            if not stack or stack[-1] != '(':
+                return False
+            stack.pop()
+            
+        elif i == '}':
+            if not stack or stack[-1] != '{':
+                return False
+            stack.pop()
+            
+        elif i == ']':
+            if not stack or stack[-1] != '[':
+                return False
+            stack.pop()
+            
+    return len(stack) == 0
+            
+def solution(s):
+    cnt = 0
+    for _ in range(len(s)):
+        if is_valid(s):
+            cnt += 1
+        
+        s = s[1:] + s[0]
+    
+    return cnt
