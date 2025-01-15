c3e6d2b2e1ba25c6302ffa180869dd10ef57bff9
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-01-15 19:03:03 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int[] num_list, int n) {
         
-        for(int i=0; i<num_list.length; i++) {
+        for (int i=0; i<num_list.length; i++) {
             if(num_list[i] == n) {
                 return 1;
+            } else {
+                return 0;
             }
         }
-        
-        return 0;
     }
 }
