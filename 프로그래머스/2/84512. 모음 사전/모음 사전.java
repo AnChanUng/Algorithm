e873464a973b0451b38e1a0ca0e226e22c581d9b
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-01-26 23:50:27 +0900

    [level 2] Title: 모음 사전, Time: 0.28 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -6,7 +6,9 @@ class Solution {
         int index, result = word.length();
         for(int i=0; i<word.length(); i++) {
             index = str.indexOf(word.charAt(i));
+            System.out.println(index);
             result += x[i] * index;
+            //System.out.println(result);
         }
         
         return result;
