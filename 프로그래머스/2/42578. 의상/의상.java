5de9f8e9e7514ecffb2c71b5d13d0c5be027085a
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-01-27 14:59:10 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -7,9 +7,13 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+            if(!map.containsKey(clothes[i][1])) {
+                map.put(clothes[i][1], 1);
+            } else {
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            }
         }
-        
+
         for(int count : map.values()) {
             answer = answer * (count+1);
         }
