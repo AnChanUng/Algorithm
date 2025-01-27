cef7a61ed41f874521673f196c5d521e7ed24ab8
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-01-27 22:34:19 +0900

    [Silver V] Title: 단어 정렬, Time: 296 ms, Memory: 25848 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,27 +3,35 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        ArrayList<String> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[num];
+
         for(int i=0; i<num; i++) {
-            String str = sc.next();
-            if(!list.contains(str)) {
-                list.add(str);
-            }
+            arr[i] = br.readLine();
         }
-        
-        Collections.sort(list, (s1, s2) -> {
-            int lengthCompare = Integer.compare(s1.length(), s2.length());
-            if (lengthCompare != 0) {
-                return lengthCompare;
+
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
             }
-            return s1.compareTo(s2);
         });
-           
-        for(String str : list) {
-            System.out.println(str);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(arr[0]).append('\n');
+
+        for(int i=1; i<num; i++) {
+            if(!arr[i].equals(arr[i-1])) {
+                sb.append(arr[i]).append('\n');
+            }
         }
+        System.out.println(sb);
     }
 }
