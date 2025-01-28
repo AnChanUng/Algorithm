849444c44bafcca7f7c8af810a89dc0b2fdcf175
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-01-29 02:14:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 580 ms, Memory: 51564 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -6,12 +6,13 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         int[][] arr = new int[n][2];
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0; i<n; i++) {
-            String[] inputs = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(inputs[0]);
-            arr[i][1] = Integer.parseInt(inputs[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr, (o1, o2) -> {
@@ -23,8 +24,8 @@ class Main {
         });
 
         for(int i=0; i<n; i++) {
-            System.out.print(arr[i][0] + " ");
-            System.out.println(arr[i][1]);
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
+        System.out.println(sb);
     }
 }
