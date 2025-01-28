f3d44fd19e64887fbed42f0773d8b611fbc24bd6
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-01-28 16:01:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 86128 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int number = 0;
+        while(true) {
+            number++;
+            if(String.valueOf(number).contains("666")) {
+                cnt++;
+            }
+
+            if(cnt == n) {
+                System.out.println(number);
+                break;
+            }
+        }
+    }
+}
