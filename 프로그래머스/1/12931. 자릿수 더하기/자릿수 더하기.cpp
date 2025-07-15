16a2548f274ead6581a1312a62767d94c649c984
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-07-15 20:00:30 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string str = to_string(n);
+    for(int i=0; i<str.size(); i++) {
+        answer += str[i] - '0';
+    }
+
+    return answer;
+}
