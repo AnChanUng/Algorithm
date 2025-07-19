154965d22e25fd02e7041a282f9fe95a6018acc5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-07-19 11:54:10 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    
+    if(s.size() != 4 && s.size() != 6) {
+        return false;
+    }
+    
+    for(char c : s) {
+        if(!isdigit(c)) {
+            return false;
+        }
+    }
+    
+    return answer;
+}
