dfaf25908a246194b2c0ebbd5e0ecc1b0f03ee61
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-08-15 10:21:12 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,36 +1,32 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <queue>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> workSpeed;
-    int cnt = 1;
+    queue<int> q;
+    
     for(int i=0; i<progresses.size(); i++) {
-        int n = 1;
-        while(true) {
-            if(((100 - progresses[i]) - (speeds[i] * n)) <= 0) {
-                workSpeed.push_back(n);
-                break;
-            }
-            n++;
-        }
+        int temp = (100 - progresses[i]) % speeds[i];
+        if(temp == 0) 
+            q.push((100-progresses[i]) / speeds[i]);
+        else 
+            q.push((100-progresses[i]) / speeds[i] + 1);
     }
-    int pivot = workSpeed[0];
-    for(int i=1; i<workSpeed.size(); i++) {
-        // 5 10 1 1 20 1 
-        if(pivot < workSpeed[i]) {
-            answer.push_back(cnt);
-            pivot = workSpeed[i];
-            cnt = 1;
-        } else {
+     
+    while(!q.empty()) {
+        int cnt = 1;
+        int cur = q.front();
+        q.pop();
+        
+        while(cur >= q.front() && !q.empty()) {
+            q.pop();
             cnt++;
         }
+        
+        answer.push_back(cnt);
     }
-    answer.push_back(cnt);
-    // for(int i=0; i<workSpeed.size(); i++) {
-    //     cout << workSpeed[i] << " ";
-    // }
+    
     return answer;
 }
