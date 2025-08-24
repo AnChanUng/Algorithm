aed0e4ccf84fdf5f48f8cfcfac324cc31d158c98
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-08-24 13:06:53 +0900

    [level 2] Title: 소수 찾기, Time: 0.98 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 98.8 MB, 시간: 32.47 ms
+메모리: 4.2 MB, 시간: 0.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 25일 23:09:42
+2025년 08월 24일 13:06:51
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+set<int> numberSet;
+
+bool isPrime(int number) {
+    if(number <= 1) {
+        return false;
+    }
+    
+    for(int i=2; i*i<=number; i++) {
+        if(number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void makeCombination(string comb, string others) {
+    if(comb != "") {
+        numberSet.insert(stoi(comb));
+    }
+    
+    for(int i=0; i<others.size(); i++) {
+        makeCombination(comb + others[i], others.substr(0, i) + others.substr(i + 1));
+    }
+}
+
+int solution(string numbers) {
+    makeCombination("", numbers);
+    
+    int answer = 0;
+    for(int number : numberSet) {
+        if(isPrime(number)) {
+            answer++;
+        }
+    }    
+
+    return answer;
+}
