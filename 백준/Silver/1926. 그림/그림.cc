d97ff84394a0977a260628d912605b96a55ce3c5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-08-26 00:02:02 +0900

    [Silver I] Title: 그림, Time: 28 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -4,9 +4,9 @@ using namespace std;
 #define Y second
 int board[502][502];
 bool vis[502][502];
-int n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+int n, m;
 
 int main(void) {
     ios::sync_with_stdio(0);
@@ -14,15 +14,15 @@ int main(void) {
     cin >> n >> m;
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
-            cin >> board[i][j];        
+            cin >> board[i][j];
         }
     }
     int mx = 0;
     int num = 0;
-
+    
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
-            if(board[i][j] == 0 || vis[i][j]) continue;
+            if(vis[i][j] || board[i][j] == 0) continue;
             num++;
             queue<pair<int, int>> q;
             vis[i][j] = 1;
@@ -32,13 +32,13 @@ int main(void) {
                 area++;
                 pair<int, int> cur = q.front(); q.pop();
                 for(int dir=0; dir<4; dir++) {
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if(vis[nx][ny] || board[nx][ny] != 1) continue;
-                    vis[nx][ny] = 1;
-                    q.push({nx, ny});
-                }
+                     int nx = cur.X + dx[dir];
+                     int ny = cur.Y + dy[dir];
+                     if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                     if(vis[nx][ny] || board[nx][ny] != 1) continue;
+                     vis[nx][ny] = 1;
+                     q.push({nx, ny});
+                }                   
             }
             mx = max(mx, area);
         }
