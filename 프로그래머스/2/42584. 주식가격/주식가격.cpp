1a5f70bd2f57ceeaa688382dbee4ffb1507ced86
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-08-31 23:55:49 +0900

    [level 2] Title: 주식가격, Time: 27.51 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,11 +1,10 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.size(), 0);
-
+    vector<int> answer;
     for(int i=0; i<prices.size(); i++) {
         int time = 0;
         for(int j=i+1; j<prices.size(); j++) {
@@ -13,8 +12,8 @@ vector<int> solution(vector<int> prices) {
             if(prices[j] < prices[i]) {
                 break;
             }
-        }
-        answer[i] = time;
+        } 
+        answer.push_back(time);
     }
     return answer;
 }
