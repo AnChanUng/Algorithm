2c4a51580c4b172c018624ac88864ea5e1e93777
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-08-31 23:35:55 +0900

    [Gold III] Title: 감시, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,4 +1,3 @@
-// http://boj.kr/c961e6bf6107428caf200c11c964f9e1
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
@@ -9,42 +8,36 @@ int board1[10][10];
 int board2[10][10];
 int n, m;
 vector<pair<int, int>> cctv;
-bool OOB(int a, int b) {
-    return a < 0 || a >= n || b < 0 || b >= m;
-}
-// (x, y)에서 dir 방향으로 진행하며 벽을 만날 때 까지 지나치는 모든 빈칸을 7로 바꿈
 void upd(int x, int y, int dir) {
     dir %= 4;
     while(true) {
         x += dx[dir];
         y += dy[dir];
-        if(OOB(x, y) || board2[x][y] == 6) return; // 범위를 벗어났거나 벽을 만나면 함수 탈출
-        if(board2[x][y] != 0) continue; // 해당칸이 빈칸이 아니면(cctv가 있으면) continue
-        board2[x][y] = 7; // 빈칸을 7로 덮음
+        if(x < 0 || x >= n || y < 0 || y >= m) return;
+        if(board2[x][y] == 6) return;
+        if(board2[x][y] != 0) continue;
+        board2[x][y] = 7;
     }
 }
 
-int main() {
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-    int mn = INT_MAX; // 사각 지대의 최소 크기
-    for(int i=0; i<n; i++ ){
+    int mn = 0;
+    for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
             cin >> board1[i][j];
             if(board1[i][j] != 0 && board1[i][j] != 6) {
                 cctv.push_back({i, j});
             }
-            if(board1[i][j] == 0) {
-                mn++;;
-            }
+            if(board1[i][j] == 0) mn++;
         }
     }
-    // 1 << (2*cctv.size())는 4의 cctv.size()승을 의미
-    for(int tmp=0; tmp<(1<<(2*cctv.size())); tmp++) { // tmp를 4진법으로 뒀을 때 각 자리수를 cctv 방향으로 생각
+    for(int tmp=0; tmp<(1<<(2*cctv.size())); tmp++) {
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                board2[i][j] = board1[i][j];
+                board2[i][j] = board1[i][j];                    
             }
         }
         int brute = tmp;
@@ -65,17 +58,19 @@ int main() {
                 upd(x, y, dir);
                 upd(x, y, dir+1);
                 upd(x, y, dir+2);
-            } else { // board1[x][y] == 5
+            } else {
                 upd(x, y, dir);
                 upd(x, y, dir+1);
                 upd(x, y, dir+2);
                 upd(x, y, dir+3);
             }
         }
-        int val = 0;
-        for(int i=0; i<n; i++) 
-            for(int j=0; j<m; j++) 
+        int val=0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 val += (board2[i][j] == 0);
+            }
+        }
         mn = min(mn, val);
     }
     cout << mn;
