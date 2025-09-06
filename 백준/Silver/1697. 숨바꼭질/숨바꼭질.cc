f1c35f305edbfc5fd606b1e5eac49e63b634e04e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-09-06 15:53:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -2,23 +2,22 @@
 using namespace std;
 #define X first
 #define Y second
-int dist[100002];
-int n, k;
+int dist[100001];
+int n, k; // 수빈의 위치 동생의 위치
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
     cin >> n >> k;
     fill(dist, dist+100001, -1);
-    dist[n] = 0;
     queue<int> q;
     q.push(n);
-    while(dist[k] == -1) {
-        int cur = q.front(); q.pop();
-        for(int nxt : {cur-1, cur+1, 2*cur}) {
-            if(nxt < 0 || nxt > 100000) continue;
-            if(dist[nxt] != -1) continue;
-            dist[nxt] = dist[cur] + 1;
-            q.push(nxt);
+    dist[n] = 0;
+    while(!q.empty()) {
+        auto cur = q.front(); q.pop();
+        if(cur == k) break;
+        for(int nx : {cur-1, cur+1, cur*2}) {
+            if(nx < 0 || nx > 100000) continue;
+            if(dist[nx] != -1) continue;
+            dist[nx] = dist[cur] + 1;
+            q.push(nx);
         }
     }
     cout << dist[k];
