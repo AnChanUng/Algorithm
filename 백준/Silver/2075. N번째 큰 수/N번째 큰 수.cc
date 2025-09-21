0be9b33a8bcd543dce10861428bf9a220c232936
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-09-21 21:16:04 +0900

    [Silver III] Title: N번째 큰 수, Time: 320 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, x;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> x;
+            pq.push(x);
+            if(pq.size() > n) {
+                pq.pop();
+            }
+        }
+    }
+    cout << pq.top();
+}
