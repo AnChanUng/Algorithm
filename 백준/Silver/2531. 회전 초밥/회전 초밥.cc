0daaba6dddc7f42e2cb145cac0bbc08a6732bf90
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-09-22 23:47:54 +0900

    [Silver I] Title: 회전 초밥, Time: 468 ms, Memory: 2256 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, d, k, c;
+int arr[30002];
+int check[30002];
+int maxCnt = 0;
+int main() {
+    cin >> n >> d >> k >> c;
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        int flag = 0;
+        int coupon = 1;
+        for(int j=i; j<i+k; j++) {
+            if(check[arr[j % n]] == false) {
+                check[arr[j % n]] = true;
+            } else {
+                flag++;
+            }
+        }
+        if(check[c] == true) {
+            coupon = 0;
+        }
+        maxCnt = max(maxCnt, k - flag + coupon);
+        memset(check, false, sizeof(check));
+    }
+    cout << maxCnt << "\n";
+}
