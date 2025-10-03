463c05237b085b2db1bf705fecad8784881a37ea
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-10-03 11:18:38 +0900

    [Gold V] Title: A와 B 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.cc
+++ 백준/Gold/12919. A와 B 2/A와 B 2.cc
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 string s, t;
-
 bool flag = false;
 
 void dfs(string cur) {
-    if(cur.size() < s.size()) return;
-    if(cur == s) {
+    if(cur == s) { 
         flag = true;
-        return ;
+        return;
     }
+    if(cur.size() < s.size()) return;
     if(!cur.empty() && cur.back() == 'A') {
         string next = cur;
         next.pop_back();
@@ -17,8 +16,8 @@ void dfs(string cur) {
     }
     if(!cur.empty() && cur.front() == 'B') {
         string next = cur;
-        next.erase(next.begin());
         reverse(next.begin(), next.end());
+        next.pop_back();
         dfs(next);
     }
 }
@@ -27,10 +26,6 @@ int main() {
     cin >> s;
     cin >> t;
     dfs(t);
-
-    if(flag) {
-        cout << 1;
-    } else {
-        cout << 0;
-    }
+    if(flag) cout << 1;
+    else cout << 0;
 }
