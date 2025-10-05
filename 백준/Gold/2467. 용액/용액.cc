23bb4677eb271a1a8cfbb4ea049525e56eb01273
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-10-05 16:22:16 +0900

    [Gold V] Title: 용액, Time: 56 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, val;
-int minVal = INT_MAX;
-vector<long long> v;
-pair<long long, long long> ans;
+vector<int> v;
+pair<int, int> ans;
 int main() {
     cin >> n;
     for(int i=0; i<n; i++) {
         cin >> val;
-        v.push_back(val);    
+        v.push_back(val);
     }
-    int left = 0;
-    int right = n - 1;
-    long long minVal = LLONG_MAX;
+    sort(v.begin(), v.end());
     
+    int left = 0;
+    int right = n-1;
+    int maxVal = INT_MAX;
+
     while(left < right) {
-        long long sum = v[left] + v[right];
-        if(abs(sum) < minVal) {
-            minVal = abs(sum);
+        int sum = v[left] + v[right];
+        if(abs(sum) < maxVal) {
+            maxVal = abs(sum);
             ans = {v[left], v[right]};
         }
-        if(sum > 0) right--;
-        else left++;
+        if(sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
     }
-    
     cout << ans.first << " " << ans.second;
 }
