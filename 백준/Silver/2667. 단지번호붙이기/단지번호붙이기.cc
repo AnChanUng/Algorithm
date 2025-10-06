5f49f79182b2a4f53f2b4edaafadc7246f6b8dbc
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-10-07 01:30:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -8,32 +8,31 @@ int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 #define X first
 #define Y second
+
+int dfs(int x, int y) {
+    vis[x][y] = true;
+    int cnt = 1;
+    
+    for(int dir=0; dir<4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+        if(!vis[nx][ny] && graph[nx][ny] == '1') {
+            cnt += dfs(nx, ny);
+        }
+    }
+    return cnt;
+}
+
 int main() {
     cin >> n;
     for(int i=0; i<n; i++) {
         cin >> graph[i];
     }
-
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             if(graph[i][j] == '1' && !vis[i][j]) {
-                queue<pair<int, int>> q;
-                vis[i][j] = true;
-                q.push({i, j});
-                int cnt = 1;
-                while(!q.empty()) {
-                    auto cur = q.front(); q.pop();
-                    for(int dir=0; dir<4; dir++) {
-                        int nx = cur.X + dx[dir];
-                        int ny = cur.Y + dy[dir];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if(!vis[nx][ny] && graph[nx][ny] == '1') {
-                            vis[nx][ny] = true;
-                            q.push({nx, ny});
-                            cnt++;
-                        }
-                    }
-                }
+                int cnt = dfs(i, j);
                 v.push_back(cnt);
             }
         }
