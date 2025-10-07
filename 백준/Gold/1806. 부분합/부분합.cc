27ca7f1beafe7fed2ab4b52b1bf69f234332701d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-10-07 21:31:39 +0900

    [Gold IV] Title: 부분합, Time: 28 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -2,22 +2,21 @@
 using namespace std;
 int n, s, a;
 vector<int> v;
-
 int main() {
     cin >> n >> s;
     for(int i=0; i<n; i++) {
         cin >> a;
         v.push_back(a);
     }
-    int left=0, right=0;
+    int left = 0;
+    int right = 0;
     int sum = 0;
     int minLen = INT_MAX;
-    
     while(true) {
         if(sum >= s) {
             minLen = min(minLen, right - left);
             sum -= v[left++];
-        } else if (right == n) { 
+        } else if (right == n) {
             break;
         } else {
             sum += v[right++];
