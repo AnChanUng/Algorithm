fa5e8ec0fb3c922df936193dd41c0fbf04ce74fd
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-10-25 21:50:11 +0900

    [level 2] Title: 귤 고르기, Time: 243.46 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,35 +1,25 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <unordered_map>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+int total = 0; 
+int answer = 0;
 
 int solution(int k, vector<int> tangerine) {
-    int answer = 0;
-    
-    unordered_map<int, int> um;
+    int maxNum = *max_element(tangerine.begin(), tangerine.end());
+    vector<int> cnt(maxNum+1, 0);
     
     for(int i=0; i<tangerine.size(); i++) {
-        um[tangerine[i]]++;
+        cnt[tangerine[i]]++;
     }
+
+    sort(cnt.begin(), cnt.end(), greater<int>());
     
-    vector<int> cnts;
-    cnts.reserve(um.size());
-    for(auto &kv : um) {
-        cnts.push_back(kv.second);
-    }
-    
-    sort(cnts.begin(), cnts.end(), greater<int>());
-    
-    int useKinds = 0;
-    for(int c : cnts) {
-        k -= c;
-        useKinds++;
-        if(k <= 0) {
+    for(int i=0; i<cnt.size(); i++) {
+        total += cnt[i];
+        answer++;
+        if(total >= k) {
             break;
         }
     }
     
-    return useKinds;
+    return answer;
 }
