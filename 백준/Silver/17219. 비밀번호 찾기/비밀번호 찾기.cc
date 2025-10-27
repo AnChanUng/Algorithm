21ec9b62350b28a374178f2544cd2c14b346c3d0
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-10-27 21:23:50 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 92 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
-string site, pw, pwsite;
 unordered_map<string, string> um;
+int n, m;
+string site, pw, findSite;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> n >> m;
-
     for(int i=0; i<n; i++) {
         cin >> site >> pw;
         um.insert({site, pw});
     }
 
     for(int i=0; i<m; i++) {
-        cin >> pwsite;
-        auto it = um.find(pwsite);
+        cin >> findSite;
+        auto it = um.find(findSite);
         if(it != um.end()) {
             cout << it->second << "\n";
         }
