9ccbe9e6e948931e9f268934c8fa33516d0b4d3f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-02 12:25:31 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, k, b, x;
+int n, k, b;
+int x;
 
+vector<int> broken(100001, 0);
+vector<int> psum(100001, 0);
 int main() {
     cin >> n >> k >> b;
-    vector<int> psum(n+1, 0);
-    vector<int> broken(n+1, 0);
-    
     for(int i=0; i<b; i++) {
         cin >> x;
         broken[x] = 1;
     }
-    
-    for(int i=1; i<=n; i++) {
-        psum[i] = psum[i-1] + broken[i];
+    int cur = 0;
+    for(int i=1; i<=k; i++) {
+        cur += broken[i];
     }
 
-    int answer = INT_MAX;
-    for(int i=1; i <=n-k+1; i++) {
-        int brokenCount = psum[i+k-1] - psum[i-1];
-        answer = min(answer, brokenCount);
+    int answer = cur;
+    for(int i=1; i<=n-k+1; i++) {
+        cur = cur + broken[i+k] - broken[i];
+        answer = min(answer, cur);
     }
-    
+
     cout << answer;
-    return 0;
 }
