0be1fb08b620b495cb44aa3ea92d776799e6b9ad
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-04 13:19:04 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 65 ms, Memory: 5,844 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.cpp
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+int dump, dumpCnt;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	//int T;
+
+	//cin>>T;
+
+	for(test_case = 1; test_case <= 10; ++test_case)
+	{
+		cin >> dump;
+		cin.ignore();
+
+		string line;
+		getline(cin, line);
+		
+		stringstream ss(line);
+		int x;
+		vector<int> v;
+		while(ss >> x) {
+			v.push_back(x);
+		}
+		// 가장 높은 덤프에서 가장 낮은 곳으로 옮김
+		for(int i=0; i<dump; i++) {
+			sort(v.begin(), v.end(), greater<int>());
+			v[0] = v[0] - 1;
+			v[v.size()-1] = v[v.size()-1] + 1;
+		}
+		int maxNum = *max_element(v.begin(), v.end());
+		int minNum = *min_element(v.begin(), v.end());
+		int result = maxNum - minNum;
+
+		cout << "#" << test_case << " " << result << "\n";
+	}
+	return 0;
+}
