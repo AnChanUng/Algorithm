48fcf1dd8bfb664cdc7752c92bcc3a61d50e9a15
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-14 19:18:04 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int arr[10];
+
+void dfs(int depth, int start) {
+    if(depth == m) {
+        for(int i=0; i<m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i=start; i<=n; i++) {
+        arr[depth] = i;
+        dfs(depth+1, i+1);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    dfs(0, 1);
+    return 0;
+}
