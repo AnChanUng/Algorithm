3896c759fc8f63ff332b3f7beae8d6d574a4f2c2
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-17 16:37:13 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.cpp
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.cpp
@@ -31,7 +31,7 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 #include <bits/stdc++.h>
 using namespace std;
-int wordLength;
+int wordLen;
 string word;
 
 int main(int argc, char** argv)
@@ -40,33 +40,24 @@ int main(int argc, char** argv)
 	int T;
 
 	//cin>>T;
-	/*
-		문제
-		1. 10개의 test_case를 입력받음
-		1.1 문자열의 길이와 문자열을 입력 받음
-		2. 현재 위치와 현재위치+1을 탐색, 같으면 삭제 후 위치 -1
-		2.1 다르면 continue
-
-		접근 방식
-		- 완전탐색으로 현재 위치의 문자열과 오른쪽의 문자열이 같으면 
-		- 두 문자열을 삭제 후, 현재 위치-1 하고 다시 탐색
-	*/
+	
     for(test_case = 1; test_case <= 10; ++test_case)
 	{
-		cin >> wordLength >> word;
-
-		string tmp = word;
+		cin >> wordLen>> word;
+		
 		int idx = 0;
-		while(idx+1 < tmp.size()) {
-			if(tmp[idx] == tmp[idx+1]) {
-				tmp.erase(idx, 2);
-				if(idx > 0) idx--;
+		string str = word;
+		while(idx < str.size()) {
+			//cout << "idx: " << idx << " " << str.size() << "\n";
+			if(str[idx] == str[idx+1]) {
+				//cout << str << "\n";
+				str.erase(idx, 2);
+				if(idx > 0) { idx--; }
 			} else {
 				idx++;
 			}
 		}
-
-		cout << "#" << test_case << " " << tmp << "\n";
+		cout << "#" << test_case << " " << str << "\n";
 	}
 	return 0;
 }
