0d0868e90d7a1068e0b3f678afefe6b67b94510f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-17 20:55:11 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
-int arr[10];
+int arr[9];
+/*
+	중복 있는 조합
+*/
 
 void dfs(int depth, int start) {
-    if(depth == m) {
-        for(int i=0; i<m; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-        return;
-    }
-    for(int i=start; i<=n; i++) {
-        arr[depth] = i;
-        dfs(depth+1, i);
-    }
+	if(depth == m) {
+		for(int i=0; i<m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for(int i=start; i<=n; i++) {
+		arr[depth] = i;
+		dfs(depth+1, i);
+	}
 }
 
-int main() {
-    cin >> n >> m;
-    dfs(0, 1);
-    return 0;
+int main(int argc, char** argv)
+{   
+	cin >> n >> m;
+	dfs(0, 1);
+	return 0;
 }
