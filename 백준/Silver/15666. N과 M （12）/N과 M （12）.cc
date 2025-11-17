fa1ded559178715644d7210560c2ef422e9df9b6
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-17 21:39:40 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m, nn;
-int arr[10];
-bool vis[10];
 vector<int> v;
+int arr[9];
 /*
-    중복 제거 조합
+	중복 없는 조합
 */
-
 void dfs(int depth, int start) {
-    if(depth == m) {
-        for(int i=0; i<m; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-        return;
-    }
-    int used = INT_MIN;
+	if(depth == m) {
+		for(int i=0; i<m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	int used = INT_MAX;
 
-    for(int i=start; i<n; i++) {
-        if(used != v[i]) {
-            arr[depth] = v[i];
-            used = v[i];
-            dfs(depth+1, i);
-        }    
-    }
+	for(int i=start; i<n; i++) {
+		if(used != v[i]) {
+			arr[depth] = v[i];
+			used = v[i];
+			dfs(depth+1, i);
+		}
+	}
 }
 
-int main() {
-    cin >> n >> m;
-    for(int i=0; i<n; i++) {
-        cin >> nn;
-        v.push_back(nn);
-    }
-    sort(v.begin(), v.end());
-    dfs(0, 0);
-    return 0;
+int main(int argc, char** argv)
+{   
+	cin >> n >> m;
+	for(int i=0; i<n; i++) {
+		cin >> nn;
+		v.push_back(nn);
+	}
+	sort(v.begin(), v.end());
+	dfs(0, 0);
+	return 0;
 }
