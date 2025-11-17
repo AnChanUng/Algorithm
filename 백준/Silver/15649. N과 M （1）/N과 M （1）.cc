614f8575de1a3679e7e96931c70e3db33b1a07f2
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-17 20:25:41 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
-int arr[10];
-bool visited[10];
+int arr[9];
+bool vis[9];
 
 void dfs(int depth) {
-    if(depth == m) {
-        for(int i=0; i<m; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-        return;
-    }
-
-    for(int i=1; i<=n; i++) {
-        if(!visited[i]) {
-            visited[i] = true;
-            arr[depth] = i;
-            dfs(depth+1);
-            visited[i] = false;
-        }
-    }
+	if(depth == m) {
+		for(int i=0; i<m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for(int i=1; i<=n; i++) {
+		if(!vis[i]) {
+			arr[depth] = i;
+			vis[i] = true;
+			dfs(depth+1);
+			vis[i] = false;
+		}
+	}
 }
 
-int main() {
-    cin >> n >> m;
-
-    dfs(0);
-
-    return 0;
+int main(int argc, char** argv)
+{   
+	/*
+		수열
+	*/ 
+	cin >> n >> m;
+	dfs(0);
+	return 0;
 }
