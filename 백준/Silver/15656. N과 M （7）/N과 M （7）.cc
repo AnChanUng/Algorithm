ccd6bd77a9b82e7aeabe689d9d12a4a9e0eaacea
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-17 21:11:51 +0900

    [Silver III] Title: N과 M (7), Time: 556 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m, nn;
-int arr[8];
-int vis[8];
 vector<int> v;
-
-void dfs(int depth) {
-    if(depth == m) {
-        for(int i=0; i<m; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-        return;
-    }
-    for(int i=0; i<n; i++) {
-        arr[depth] = v[i];
-        dfs(depth+1);
-    }
+int arr[8];
+/*
+	중복 가능 수열
+*/
+void dfs(int depth, int start) {
+	if(depth == m) {
+		for(int i=0; i<m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for(int i=0; i<n; i++) {
+		arr[depth] = v[i];
+		dfs(depth+1, i);
+	}
 }
 
-int main() {
-    cin >> n >> m;
-    for(int i=0; i<n; i++) {
-        cin >> nn;
-        v.push_back(nn);
-    }
-    sort(v.begin(), v.end());
-    
-    dfs(0);
-
-    return 0;
+int main(int argc, char** argv)
+{   
+	cin >> n >> m;
+	for(int i=0; i<n; i++) {
+		cin >> nn;
+		v.push_back(nn);
+	}
+	sort(v.begin(), v.end());
+	dfs(0, 0);
+	return 0;
 }
