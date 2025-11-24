99048c1d097d7c19a6660302bd1591bee3f4215d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-24 17:57:03 +0900

    [Bronze II] Title: 벌집, Time: 184 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int layer = 1;
+        int end = 1;
+        while(n > end) { 
+            end += layer * 6;
+            layer++;
+        }
+
+        System.out.println(layer);
+    }
+}
