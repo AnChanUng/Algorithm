b02058d71af6a67ed9c28a7febb7db17b12526f5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-25 00:41:38 +0900

    [Bronze I] Title: 팰린드롬수, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String str = br.readLine();
+            StringBuilder sb = new StringBuilder(str);
+
+            if(str.equals("0")) break;
+
+            String reverseStr = sb.reverse().toString();
+
+            if(str.equals(reverseStr)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
