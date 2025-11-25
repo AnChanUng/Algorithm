3654ae85c7a85dcbe75f89aa8aa0c9d3f5d3e598
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-25 22:28:45 +0900

    [Bronze I] Title: FizzBuzz, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String num1 = br.readLine();
+        String num2 = br.readLine();
+        String num3 = br.readLine();
+
+        int num;
+
+        if(isNumber(num1)) num = Integer.parseInt(num1) + 3;
+        else if (isNumber(num2)) num = Integer.parseInt(num2) + 2;
+        else num = Integer.parseInt(num3) + 1;
+
+        System.out.println(getFizzBuzzNumber(num));
+    }
+
+    static boolean isNumber(String str) {
+        return !str.equals("Fizz") && !str.equals("Buzz") && !str.equals("FizzBuzz");
+    }
+
+    static String getFizzBuzzNumber(int num) {
+        if(num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+        else if (num % 3 == 0) return "Fizz";
+        else if (num % 5 == 0) return "Buzz";
+        else return Integer.toString(num);
+    }
+}
