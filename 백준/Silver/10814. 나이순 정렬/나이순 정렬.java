8df3b68bbfa5054a25bc4fcbe22edbfa089eb8a3
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-26 20:50:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 604 ms, Memory: 54236 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,24 +1,44 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-class Main {  
+class Person implements Comparable<Person> {
+    int age;
+    String name;
+
+    Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return this.age - other.age;
+    }
+}
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
+        Person[] people = new Person[n];
 
-        String[][] arr = new String[n][2];
         for(int i=0; i<n; i++) {
-            String[] inputs = br.readLine().split(" ");
-            arr[i][0] = inputs[0];
-            arr[i][1] = inputs[1];
+            String[] parts = br.readLine().split(" ");
+            int age = Integer.parseInt(parts[0]);
+            String name = parts[1];
+            people[i] = new Person(age, name);
         }
 
-        Arrays.sort(arr, (o1, o2) -> Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]));
-        
-        for(int i=0; i<n; i++) {
-            System.out.print(arr[i][0] + " ");
-            System.out.println(arr[i][1]);
+        Arrays.sort(people);
+
+        StringBuilder sb = new StringBuilder();
+        for(Person p : people) {
+            sb.append(p.age).append(" ").append(p.name).append("\n");
         }
+
+        System.out.print(sb);
     }
 }
