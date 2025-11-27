d0adf790f83ea2293d0bfb62f174ef742df9d812
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-28 02:05:05 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 240 ms, Memory: 19772 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -4,13 +4,16 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 /*
     괄호(,[ 들의 균형이 잘 맞춰져 있는지 판단하는 프로그램
-    (), [] 짝을 이뤄야함
+    - (), [] 짝을 이뤄야함
+    - 입력은 여러 줄, 단독 '.' 입력 시 종료
 
     문제
     1.1 문자열을 .가 나올때까지 입력받는다.
     1.2 마지막에 .가 한개 나오면 입력을 종료한다.
+    2   각 문자를 순회하며
     2.1 (나 [가 나오면 stack에 저장한다.
     2.2 )나 ]가 나오면 stack.pop()한다
+    2.2 다른 문자는 무시
     3   stack에 아무것도 없으면 yes를 출력한다 아니면 no를 출력한다
  */
 public class Main {
@@ -19,26 +22,20 @@ public class Main {
 
         while(true) {
             String str = br.readLine();
-            Stack<Character> stack = new Stack<>();
-            boolean flag2 = false;
             if(str.equals(".")) break;
+            
+            Stack<Character> stack = new Stack<>();
+            boolean flag = false;
             for(int i=0; i<str.length(); i++) {
                 char st = str.charAt(i);
                 if (st == '.') continue;
-                if (stack.empty()) {
-                    if(st == ')' || st == ']') {
-                        flag2 = true;
-                        System.out.println("no");
-                        break;
-                    }
-                }
                 if (st == '(' || st == '[') {
                     stack.push(st);
                 } else if (st == ')') {
                     if (!stack.empty() && stack.peek() == '(') {
                         stack.pop();
                     } else {
-                        flag2 = true;
+                        flag = true;
                         System.out.println("no");
                         break;
                     }
@@ -46,13 +43,13 @@ public class Main {
                     if (!stack.empty() && stack.peek() == '[') {
                         stack.pop();
                     } else {
-                        flag2 = true;
+                        flag = true;
                         System.out.println("no");
                         break;
                     }
                 }
             }
-            if(flag2) continue;
+            if(flag) continue;
             if(stack.isEmpty()) {
                System.out.println("yes");
             } else {
