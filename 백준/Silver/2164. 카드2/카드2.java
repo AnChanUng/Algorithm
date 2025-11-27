fe13e0874ac311a795fdc81af496f5cffa6bb2a2
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-27 20:11:09 +0900

    [Silver IV] Title: 카드2, Time: 252 ms, Memory: 39924 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,25 +1,34 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
 
-class Main {  
+/*
+    문제
+    1. n 입력 받기
+    2. 큐에 1 ~ n에 저장
+    3. 큐에 1개 남을때 까지 무한루프
+    3.1 - 맨 앞에 있는 수 삭제
+    3.2 - 맨 앞에 있는 수 맨 뒤로 옮기기
+    4. 1개 남은 수 출력
+*/
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
 
-        Queue<Integer> q = new LinkedList<>();
-        
+        int n = sc.nextInt();
+        Queue<Integer> q = new ArrayDeque<>();
         for(int i=1; i<=n; i++) {
             q.offer(i);
         }
 
-        while(q.size() > 1) {
+        while(q.size() != 1) {
             q.poll();
-            q.offer(q.poll());
+            int qq = q.peek();
+            q.poll();
+            q.offer(qq);
         }
 
-        for(int a : q) {
-            System.out.print(a);
-        }
+        System.out.print(q.peek());
     }
 }
