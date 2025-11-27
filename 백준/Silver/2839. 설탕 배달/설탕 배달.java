c0a871f7a84800895fdd9def2f720807b9af9ca0
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-27 22:50:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,31 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+/*
+    3kg or 5kg을 선택해 n킬로그램을 만들고 하나 선택할 때마다 한개씩 카운트
 
-class Main {  
+    문제
+    1. n 입력 받기
+    2. while(n != 0)
+    2.1 - n을 5로 나눔 cnt++
+    2.2 - n - 3 cnt++
+    3. 선택 개수(cnt) 출력
+ */
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
         int cnt = 0;
-        while(n > 0) {
+        while(n >= 0) {
             if(n % 5 == 0) {
                 cnt += n/5;
-                break;
+                System.out.println(cnt);
+                return;
             } else {
                 n -= 3;
                 cnt++;
             }
-
-            if(n < 0) {
-                cnt = -1;
-            }
         }
-
-        System.out.print(cnt);
+        System.out.println(-1);
     }
 }
