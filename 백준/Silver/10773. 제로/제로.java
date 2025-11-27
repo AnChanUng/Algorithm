77805da71d1afcbc484bd818dbc50cacc8e9ec3d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-28 02:49:48 +0900

    [Silver IV] Title: 제로, Time: 220 ms, Memory: 23912 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,29 +1,31 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-class Main {
+/*
+    0을 부르면 가장 마지막 수를 지움
+    배열에 있는 수 합
+ */
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int k = Integer.parseInt(br.readLine());
 
         Stack<Integer> stack = new Stack<>();
         for(int i=0; i<k; i++) {
-            int number = Integer.parseInt(br.readLine());
-
-            if(number != 0) {
-                stack.push(number);
-            } else if(!stack.isEmpty() && number == 0) {
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0) {
                 stack.pop();
+            } else {
+                stack.push(n);
             }
         }
-
-        int sum = 0;
-        for(int a : stack) {
-            sum += a;
+        int total = 0;
+        while(!stack.empty()) {
+            total += stack.peek();
+            stack.pop();
         }
-
-        System.out.println(sum);
+        System.out.println(total);
     }
 }
