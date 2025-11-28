76e328c69b697e77ca350c0c1ea9d818a96ca98c
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-11-28 16:46:51 +0900

    [Silver IV] Title: 큐, Time: 276 ms, Memory: 20740 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,48 +1,45 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Queue<Integer> q = new LinkedList<>();
-
+        Deque<Integer> q = new ArrayDeque<>();
         for(int i=0; i<n; i++) {
             String[] inputs = br.readLine().split(" ");
-
             if(inputs[0].equals("push")) {
-                q.offer(Integer.parseInt(inputs[1]));
-            } else if(inputs[0].equals("pop")) {
-                if(!q.isEmpty()) {
-                    System.out.println(q.poll());
-                } else {
+                q.offer(Integer.valueOf(inputs[1]));
+            } else if (inputs[0].equals("pop")) {
+                if(q.isEmpty()) {
                     System.out.println(-1);
+                } else {
+                    System.out.println(q.peek());
+                    q.poll();
                 }
-            } else if(inputs[0].equals("size")) {
+            } else if (inputs[0].equals("size")) {
                 System.out.println(q.size());
-            } else if(inputs[0].equals("empty")) {
+            } else if (inputs[0].equals("empty")) {
                 if(q.isEmpty()) {
                     System.out.println(1);
                 } else {
                     System.out.println(0);
                 }
-            } else if(inputs[0].equals("front")) {
-                if(!q.isEmpty()) { 
-                    System.out.println(q.peek());
-                } else {
+            } else if (inputs[0].equals("front")) {
+                if(q.isEmpty()) {
                     System.out.println(-1);
+                } else {
+                    System.out.println(q.peekFirst());
                 }
-            } else if(inputs[0].equals("back")) {
-                if(!q.isEmpty()) {
-                    Integer lastElement = null;
-                    for(Integer a : q) {
-                        lastElement = a;
-                    }
-                    System.out.println(lastElement);
-                } else {
+            } else if (inputs[0].equals("back")) {
+                if(q.isEmpty()) {
                     System.out.println(-1);
+                } else {
+                    System.out.println(q.peekLast());
                 }
             }
         }
