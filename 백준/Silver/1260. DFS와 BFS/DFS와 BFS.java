8530aaa10f5c8848afe19dabee6ec8dddec76bfc
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-12-31 21:54:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 23224 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static int[][] graph;
     static boolean[] vis;
     static int n, m, v;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken()); // 정점의 개수
         m = Integer.parseInt(st.nextToken()); // 간선의 개수
-        v = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
+        v = Integer.parseInt(st.nextToken());
 
         graph = new int[1002][1002];
         for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a][b] = 1;
-            graph[b][a] = 1;
+            graph[a][b] = graph[b][a] = 1;
         }
         vis = new boolean[1002];
         dfs(v);
-        System.out.println();
-        vis = new boolean[1002];
+        sb.append("\n");
+        Arrays.fill(vis, false);
         bfs(v);
+        System.out.println(sb);
     }
 
     static void dfs(int start) {
         vis[start] = true;
-        System.out.print(start + " ");
+        sb.append(start).append(" ");
         for(int i=1; i<=n; i++) {
             if(graph[start][i] == 1 && !vis[i]) {
                 dfs(i);
@@ -43,11 +46,11 @@ public class Main {
 
     static void bfs(int start) {
         Queue<Integer> q = new LinkedList<>();
+        q.offer(v);
         vis[start] = true;
-        q.offer(start);
         while(!q.isEmpty()) {
             int cur = q.poll();
-            System.out.print(cur + " ");
+            sb.append(cur).append(" ");
             for(int i=1; i<=n; i++) {
                 if(graph[cur][i] == 1 && !vis[i]) {
                     vis[i] = true;
