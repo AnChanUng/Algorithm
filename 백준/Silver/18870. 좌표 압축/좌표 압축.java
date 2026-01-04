f9ff0c3aab97bc219b61880f8de51686c3126732
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-01-04 22:10:24 +0900

    [Silver II] Title: 좌표 압축, Time: 1732 ms, Memory: 265056 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
@@ -13,20 +14,23 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] sortArr = Arrays.copyOf(arr, n);
+        int[] sortArr = arr.clone();
 
         Arrays.sort(sortArr);
-
+        // 2 4 -10 4 -9
+        // -10 -9 2 4 4
         Map<Integer, Integer> map = new HashMap<>();
         int rank = 0;
         for(int i=0; i<n; i++) {
             if(!map.containsKey(sortArr[i])) {
-                map.put(sortArr[i], rank++);
+                map.put(sortArr[i], rank);
+                rank++;
             }
         }
         for(int i=0; i<n; i++) {
             sb.append(map.get(arr[i])).append(" ");
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 }
