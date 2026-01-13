0987522aa86010469685f128b714d865dd7616a6
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-01-13 19:36:42 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 316 ms, Memory: 36476 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -2,56 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static char[][] graph;
+    static boolean[][] vis;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
+    static int n, m;
+    static int cnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        boolean[][] vis = new boolean[n][m];
-        char[][] graph = new char[n][m];
-
+        graph = new char[n][m];
+        vis = new boolean[n][m];
         for(int i=0; i<n; i++) {
             String line = br.readLine();
             for(int j=0; j<m; j++) {
                 graph[i][j] = line.charAt(j);
             }
         }
-
-        int cnt = 0;
+        int x = 0;
+        int y = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(graph[i][j] == 'I') {
-                    Queue<int[]> q = new LinkedList<>();
-                    q.offer(new int[]{i, j});
-                    vis[i][j] = true;
-                    while(!q.isEmpty()) {
-                        int[] cur = q.poll();
-                        int x = cur[0];
-                        int y = cur[1];
-                        for(int dir=0; dir<4; dir++) {
-                            int nx = x + dx[dir];
-                            int ny = y + dy[dir];
-                            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                            if(vis[nx][ny]) continue;
-                            if(graph[nx][ny] == 'X') continue;
-                            if(graph[nx][ny] == 'P') {
-                                cnt++;
-                            }
-                            q.offer(new int[]{nx, ny});
-                            vis[nx][ny] = true;
-                        }
-                    }
+                    x = i;
+                    y = j;
                 }
             }
         }
+        cnt = 0;
+        bfs(x, y);
         if(cnt == 0) {
             System.out.println("TT");
         } else {
             System.out.println(cnt);
+        }
+    }
+
+    static void bfs(int a, int b) {
+        Queue<int[]> q = new LinkedList<>();
+        vis[a][b] = true;
+        q.add(new int[] {a, b});
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            for(int dir=0; dir<4; dir++) {
+                int nx = cur[0] + dx[dir];
+                int ny = cur[1] + dy[dir];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(vis[nx][ny]) continue;
+                if(graph[nx][ny] == 'X') continue;
+                if(graph[nx][ny] == 'P') {
+                    cnt++;
+                }
+                vis[nx][ny] = true;
+                q.add(new int[] {nx, ny});
+            }
         }
     }
 }
