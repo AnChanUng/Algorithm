c3de073b5daf4283b812e78224e38ee19b7ee500
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-01-20 17:43:56 +0900

    [level 0] Title: ad 제거하기, Time: 0.87 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,15 +1,14 @@
 import java.util.*;
-
 class Solution {
     public String[] solution(String[] strArr) {
-    
-        List<String> list = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
+        
         for(int i=0; i<strArr.length; i++) {
-            if (!strArr[i].contains("ad")) {
-                list.add(strArr[i]);
+            if(!strArr[i].contains("ad")) {
+                answer.add(strArr[i]);
             }
         }
         
-        return list.toArray(new String[0]);
+        return answer.toArray(new String[0]);
     }
 }
