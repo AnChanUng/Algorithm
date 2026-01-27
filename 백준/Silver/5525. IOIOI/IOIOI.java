fec6471e6b94efe0be2556e2de77bd443c8f542d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-01-27 22:56:11 +0900

    [Silver I] Title: IOIOI, Time: 232 ms, Memory: 20132 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,27 +1,26 @@
 import java.io.*;
 
 public class Main {
-    static int n, m;
-    static int cnt = 0;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
-        for(int i=0; i<n; i++) {
-            sb.append("IO");
-        }
-        sb.append("I");
+        int ans = 0;
+        int k = 0; 
 
-        int cnt = 0;
-        for(int i=0; i<m-n*2; i++) {
-            String str = s.substring(i, n*2+1+i);
-            if(str.equals(sb.toString())) {
-                cnt++;
+        for (int i = 1; i < m - 1; i++) {
+            if (s.charAt(i - 1) == 'I' && s.charAt(i) == 'O' && s.charAt(i + 1) == 'I') {
+                k++;
+                if (k >= n) ans++;     
+                i++;                   
+            } else {
+                k = 0;
             }
         }
-        System.out.println(cnt);
+
+        System.out.println(ans);
     }
 }
