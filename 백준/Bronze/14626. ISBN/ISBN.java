cde58437bbce5f1c69945d49b81c7e5aff0aa210
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-02-16 12:52:35 +0900

    [Bronze I] Title: ISBN, Time: 144 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.java
+++ 백준/Bronze/14626. ISBN/ISBN.java
@@ -1,40 +1,34 @@
-import java.util.Scanner;
-/*
-    str[i]가 짝수면 1을 곱하고 i가 홀수면 3을 곱함 그 값을 10으로 나눈 나머지가 0이 되도록 한다
- */
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
         int total = 0;
-        int index = 0;
+        int a = 1;
         for(int i=0; i<str.length(); i++) {
-            if(str.charAt(i) == '*') {
-                index = i;
-                continue;
-            }
-            int n = str.charAt(i) - '0';
-            if(i % 2 == 0) {
-                total += n * 1;
+            if(str.charAt(i) != '*' && i % 2 == 0) {
+                total += str.charAt(i) - '0';   
+            } else if (str.charAt(i) != '*' && i % 2 == 1) {
+                total += (str.charAt(i) - '0') * 3;
             } else {
-                total += n * 3;
-            }
-        }
-
-        for(int i=0; i<=9; i++) {
-            if(index % 2 == 0) {
-                if ((total + i) % 10 == 0) {
-                    System.out.print(i);
-                    break;
-                }
-            } else {
-                if((total + i * 3) % 10 == 0) {
-                    System.out.print(i);
-                    break;
+                if(i % 2 == 0) {
+                    a = a * 1; 
+                } else {
+                    a = a * 3;
                 }
             }
         }
+        int r = (10 - total % 10);
+        int x = 0;
+        if(a == 1) {
+            x = r;
+        } else {
+            x = (7 * r) % 10;
+        }
+        System.out.print(x);
     }
 }
