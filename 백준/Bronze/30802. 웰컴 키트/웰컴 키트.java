8849caa017be493f4c56acdc67036310242c46ee
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-02-16 12:19:57 +0900

    [Bronze III] Title: 웰컴 키트, Time: 120 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -6,56 +6,37 @@ import java.io.*;
   펜은 남거나 부족해서는 안되고 정확한 참가자 수만큼 준비
 */
 class Main {
+    static int n, t, p;
+    static int cnt = 0;
+    static int a = 0;
+    static int b = 0;
+    static int[] arr;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
+        arr = new int[6];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
-        int xl = Integer.parseInt(st.nextToken());
-        int xxl = Integer.parseInt(st.nextToken());
-        int xxxl = Integer.parseInt(st.nextToken());
+        for(int i=0; i<6; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
         
         st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken()); // 정수 티셔츠
-        int p = Integer.parseInt(st.nextToken()); // 펜
+        t = Integer.parseInt(st.nextToken()); // 정수 티셔츠
+        p = Integer.parseInt(st.nextToken()); // 펜
         
-        int cnt = 0;
-        if(s%t == 0) {
-            cnt += s/t;
-        } else {
-            cnt += s / t + 1;
+        cnt = 0;
+        for(int i=0; i<6; i++) {
+            if(arr[i] % t == 0) {
+                cnt += arr[i] / t;
+            } else {
+                cnt += arr[i] / t + 1;
+            }
         }
-        if(m%t == 0) {
-            cnt += m / t;
-        } else {
-            cnt += m / t + 1;
-        }
-        if(l%t == 0) {
-            cnt += l / t;
-        } else {
-            cnt += l / t + 1;
-        }
-        if(xl%t == 0) {
-            cnt += xl / t;
-        } else {
-            cnt += xl / t + 1;
-        }
-        if(xxl%t == 0) {
-            cnt += xxl / t;
-        } else {
-            cnt += xxl / t + 1;
-        }
-        if(xxxl%t == 0) {
-            cnt += xxxl / t;
-        } else {
-            cnt += xxxl / t + 1;
-        }
-        
-        int a = n / p;
-        int b = n % p;
+       
+        a = n / p;
+        b = n % p;
 
         System.out.println(cnt);
         System.out.println(a + " " + b);
