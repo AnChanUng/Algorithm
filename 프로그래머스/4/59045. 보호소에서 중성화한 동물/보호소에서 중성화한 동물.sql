1170b9a4110d044c6321d9cba13fcdf016061292
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-02-20 22:12:49 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,7 +1,8 @@
-SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME # 동물의 아이디와 생물 종, 이름
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
 FROM ANIMAL_INS A JOIN ANIMAL_OUTS B
 ON A.ANIMAL_ID = B.ANIMAL_ID
-# 보호소에서 중성화 수술을 거친 동물 정보
-WHERE (A.SEX_UPON_INTAKE = 'Intact Male' OR A.SEX_UPON_INTAKE = 'Intact Female')
-AND (B.SEX_UPON_OUTCOME = 'Neutered Male' OR B.SEX_UPON_OUTCOME = 'Spayed Female')
+WHERE A.SEX_UPON_INTAKE LIKE 'Intact %' 
+AND (B.SEX_UPON_OUTCOME IN('Neutered Male') OR B.SEX_UPON_OUTCOME IN('Spayed Female'))
+# WHERE (A.SEX_UPON_INTAKE = 'Intact Male' OR A.SEX_UPON_INTAKE = 'Intact Female')
+# AND (B.SEX_UPON_OUTCOME = 'Neutered Male' OR B.SEX_UPON_OUTCOME = 'Spayed Female')
 # 보호소에 들어올 당시에는 중성화 되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물
