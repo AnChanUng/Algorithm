e9d9285cdeb6a0fe9eb24b32364e54ae39782c7b
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-10 22:34:38 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,39 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    static int[] dx = {1, 2, 3, 4, 5, 6};
-    static int[] graph;
-    static int[] sadari;
+class Main {
     static int[] bam;
+    static int[] sadari;
     static int[] dist;
-    static int n, m;
+    static int[] dx = {1, 2, 3, 4, 5, 6};
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken()); // 사다리 수
-        m = Integer.parseInt(st.nextToken()); // 뱀의 수
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        sadari = new int[101];
         bam = new int[101];
+        sadari = new int[101];
         dist = new int[101];
 
         Arrays.fill(dist, -1);
         
         for(int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine()); // 사다리정보
-            int x = Integer.parseInt(st.nextToken()); 
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             sadari[x] = y;
         }
 
         for(int i=0; i<m; i++) {
-            st = new StringTokenizer(br.readLine()); // 뱀의정보
+            st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             bam[u] = v;
         }
+
         bfs(1);
         System.out.print(dist[100]);
     }
@@ -46,18 +46,21 @@ public class Main {
             int cur = q.poll();
             for(int dir=0; dir<6; dir++) {
                 int nx = cur + dx[dir];
-                if(nx > 100 || nx <= 0) continue;
-
+                if(nx > 100) continue;
                 if(sadari[nx] != 0) {
                     nx = sadari[nx];
                 } else if(bam[nx] != 0) {
                     nx = bam[nx];
                 }
-                
+
                 if(dist[nx] == -1) {
-                    dist[nx] = dist[cur] + 1;
                     q.add(nx);
+                    dist[nx] = dist[cur] + 1;
                 }
+                //if(nx == 100) {
+                //    q.add(nx);
+                //    dist[nx] = dist[cur] + 1;
+                //}
             }
         }
     }
