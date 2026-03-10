d8f60eacbc4d7846048b7b07eac5c9285c7a0bb0
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-10 22:33:08 +0900

    [Gold V] Title: A와 B 2, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,21 +1,18 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Main {
+public class Main {
     static String s, t;
     static boolean flag = false;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         s = br.readLine();
         t = br.readLine();
 
         dfs(t);
-        if(flag) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+        if(flag) System.out.println(1);
+        else System.out.println(0);
     }
 
     static void dfs(String cur) {
@@ -23,17 +20,22 @@ class Main {
             flag = true;
             return;
         }
-        if(cur.length() < s.length()) return;
 
-        if(cur.charAt(cur.length() - 1) == 'A') {
+        if(cur.length() < s.length()) {
+            return;
+        }
+
+        // 마지막이 a면 삭제
+        if(cur.charAt(cur.length()-1) == 'A') {
             String next = cur.substring(0, cur.length()-1);
             dfs(next);
         }
+
+        // 마지막이 b면 뒤집고 삭제
         if(cur.charAt(0) == 'B') {
-            StringBuilder sb = new StringBuilder(cur);
-            sb.reverse();
-            sb.setLength(sb.length() - 1);
-            dfs(sb.toString());
+            StringBuilder sb = new StringBuilder(cur).reverse();
+            String next = sb.toString().substring(0, cur.length()-1);
+            dfs(next);
         }
     }
 }
