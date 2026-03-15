f0c8ea4f1d2d4092642ac93499e850fc6d804f62
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-16 00:22:26 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 180 ms, Memory: 19928 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -5,10 +5,17 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        /*
+        1. 입력 값 y가 현재의 y보다 크다면 새로운 건물로 추정
+         1.1 크다면 stack에 y 삽입
+         1.2 작다면 건물이 끝나는 지점 stack.pop
+         1.3 끝나는 지점이라고 추정되는 건물이 없을 때가지 반복
+         1.4 반복이 종료된 후, 건물의 높이가 이전 건물의 높이와 같지 않으면 스택에 삽입
+        2. 남은 건물의 높이가 0이 아니라면 카운트
+        */
         Stack<Integer> stack = new Stack<>();
-        int answer = 0;        
-        for(int test=0; test<n; test++) {
+        int answer = 0; // 완성된 건물 개수    
+        for(int i=0; i<n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
@@ -17,11 +24,13 @@ class Main {
                 stack.pop();
                 answer++;
             }
-
+            // 높이가 같으면 같은 건물로 추정
             if(!stack.isEmpty() && stack.peek() == y) continue;
 
+            // 이전 높이보다 크거나 비어있으면 새로운 건물로 추정 push
             stack.push(y);
         }
+        // 남은 건물 카운트
         for(int v : stack) {
             if(v == 0) {
                 continue;
