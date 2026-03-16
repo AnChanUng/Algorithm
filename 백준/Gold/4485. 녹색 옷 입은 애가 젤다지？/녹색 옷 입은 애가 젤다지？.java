59a7856e695085630e19c1ee4e4c34b23dad024d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-16 21:28:45 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 216 ms, Memory: 19588 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/*
+    (0, 0) -> (n-1, n-1) 최소 금액 이동
+    1. bfs(0, 0) 출발
+    2. 한칸 이동할 때 마다 해당 위치에 있는 숫자 만큼 cnt에 저장
+    3. arr[n-1][n-1]에 도착하면 break
+    4. cnt 출력
+*/
+class Main {
+    static int n;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static int[][] dist;
+    static int[][] arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int number = 1;
+        while(true) {
+            n = Integer.parseInt(br.readLine()); // 동굴의 크기
+            if(n == 0) break;
+
+            arr = new int[n][n];
+            dist = new int[n][n];
+            for(int i=0; i<n; i++) {
+                Arrays.fill(arr[i], 0);
+                Arrays.fill(dist[i], Integer.MAX_VALUE);
+            }
+            
+            for(int i=0; i<n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            bfs(0, 0);
+            System.out.println("Problem " + number + ": " + dist[n-1][n-1]);
+            number++;
+        }
+    }
+    static void bfs(int x, int y) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        pq.add(new int[]{x, y, arr[0][0]});
+        dist[x][y] = arr[0][0];
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            for(int dir=0; dir<4; dir++) {
+                int nx = cur[0] + dx[dir];
+                int ny = cur[1] + dy[dir]; 
+                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                int nextCost = cur[2] + arr[nx][ny];
+                if(nextCost < dist[nx][ny]) {
+                    dist[nx][ny] = nextCost;
+                    pq.add(new int[]{nx, ny, nextCost});
+                }
+            }
+        }
+    }
+}
