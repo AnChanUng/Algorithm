b17b320cdf48454368fba4e951f8cf2dfb9b1e88
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-17 00:15:54 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -7,35 +7,42 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int h = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken()); // 세로
+        int w = Integer.parseInt(st.nextToken()); // 가로
 
         int[] arr = new int[w];
+
         int maxHeight = 0;
-        int maxHeightIdx = 0;
-        int total = 0;
-        
+        int maxIdx = 0;
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<w; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            if(arr[i] > maxHeight) {
-                maxHeight = arr[i];
-                maxHeightIdx = i;
+            int a = Integer.parseInt(st.nextToken());
+            arr[i] = a;
+            if(a > maxHeight) {
+                maxHeight = a;
+                maxIdx = i;
             }
         }
 
-        int cur = 0;
-        for(int i=0; i<maxHeightIdx; i++) {
-            cur = Math.max(cur, arr[i]);
-            total += cur - arr[i];
+        // 왼쪽부터 가장 높은거 - 현재위치
+        int maxLeft = 0;
+        int maxRight = 0;
+        int total = 0;
+        for(int i=0; i<maxIdx; i++) {
+            if(maxLeft < arr[i]) {
+                maxLeft = arr[i];
+            }
+            total += maxLeft - arr[i];
         }
 
-        cur = 0;
-        for(int i=w-1; i>maxHeightIdx; i--) {
-            cur = Math.max(cur, arr[i]);
-            total += cur - arr[i];
+        // 오른쪽부터 가장 높은거 - 현재위치
+        for(int i=w-1; i>maxIdx; i--) {
+            if(maxRight < arr[i]) {
+                maxRight = arr[i];
+            }
+            total += maxRight - arr[i];
         }
-
+        
         System.out.println(total);
     }
 }
