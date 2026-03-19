a4528335f1f68af511c471e0778890628dab4594
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-19 13:26:56 +0900

    [Silver IV] Title: 수 찾기, Time: 1040 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,35 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-/*
-    시간복잡도: n = 100,000, m = 100,000;
-    O(n^2) = 10^5 * 10^5 = 10^10
- */
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+import java.util.*;
+import java.io.*;
 
-        int n = Integer.parseInt(br.readLine());
+class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
 
-        Set<Integer> arr = new HashSet<>();
-        String[] inputs1 = br.readLine().split(" ");
-        for(int i=0; i<n; i++) {
-            arr.add(Integer.parseInt(inputs1[i]));
-        }
+    int[] arr = new int[n];
 
-        int m = Integer.parseInt(br.readLine());
-        String[] inputs2 = br.readLine().split(" ");
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<m; i++) {
-            int findNum = Integer.parseInt(inputs2[i]);
-            if (arr.contains(findNum)) {
-                sb.append(1).append("\n");
-            } else {
-                sb.append(0).append("\n");
-            }
-        }
-        System.out.println(sb);
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i=0; i<n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    Arrays.sort(arr);
+
+    int m = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<m; i++) {
+      int x = Integer.parseInt(st.nextToken());
+      
+      int left = 0;
+      int right = n-1;
+      boolean flag = false;
+      while(left <= right) {
+        int mid = (left + right) / 2;
+        if(arr[mid] == x) {
+          flag = true;
+          break;
+        } else if (arr[mid] < x) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+      if(!flag) System.out.println(0);
+      else System.out.println(1);
+    }
+  }  
 }
