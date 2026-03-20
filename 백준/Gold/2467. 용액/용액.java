119a04a04708afdc7797a2495a00a8b06a8777fc
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-21 01:31:19 +0900

    [Gold V] Title: 용액, Time: 336 ms, Memory: 28716 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,38 +1,39 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
-
+// 두 용액을 더해서 가장 0에 가까운 수 두개를 출력
 class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int left=0;
-        int right=n-1;
-        int leftValue = 0;
-        int rightValue = 0;
-        int minValue = Integer.MAX_VALUE;
-        while(left < right) {
-            int sum = arr[left] + arr[right];
-            int absSum = Math.abs(sum);
-            if(absSum < minValue) {
-                minValue = absSum;
-                leftValue = arr[left];
-                rightValue = arr[right];
-            }
-            if(sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        System.out.println(leftValue + " " + rightValue);
-    }
+      int[] arr = new int[n];
+      
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int i=0; i<n; i++) {
+          arr[i] = Integer.parseInt(st.nextToken());
+      }
+      
+      int left = 0;
+      int right = n-1;
+      long minNum = Integer.MAX_VALUE;
+      int[] res = new int[2];
+      while(left < right) {         
+          int num = arr[left] + arr[right];
+          //System.out.println("left: " + left + " right: " + right + " num: " + num);
+          if(Math.abs(num) < minNum) {
+              minNum = Math.abs(num);
+              res[0] = arr[left];
+              res[1] = arr[right];
+          }
+          
+          if(num < 0) {
+              left++;
+          } else if (num > 0) {
+              right--;
+          } else {
+              break;
+          }
+      }
+      System.out.println(res[0] + " " + res[1]);
+  }
 }
