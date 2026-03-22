e1942e8254cd1c1999c00c44b8795d59bb9b8b8c
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-22 20:54:10 +0900

    [Gold IV] Title: 고층 건물, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.java
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -3,7 +3,6 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -17,24 +16,25 @@ class Main {
 
         int answer = 0;
         for(int i=0; i<n; i++) {
-            int count = 0;
-            double maxSlope = Integer.MIN_VALUE;
+            int cnt = 0;
+            double maxScope = Integer.MIN_VALUE;
             for(int j=i+1; j<n; j++) {
-                double slope = (double)(arr[j] - arr[i]) / (j - i);
-                if(slope > maxSlope) {
-                    maxSlope = slope;
-                    count++;
+                double scope = (double)(arr[j]-arr[i]) / (j - i);
+                if(scope > maxScope) {
+                    maxScope = scope;
+                    cnt++;
                 }
             }
-            maxSlope = Integer.MIN_VALUE;
+            
+            double leftScope = Integer.MIN_VALUE;
             for(int j=i-1; j>=0; j--) {
-                double slope = (double)(arr[j] - arr[i]) / (i-j);
-                if(slope > maxSlope) {
-                    maxSlope = slope;
-                    count++;
+                double scope = (double)(arr[j]-arr[i]) / (i-j);
+                if(scope > leftScope) {
+                    leftScope = scope;
+                    cnt++;
                 }
             }
-            answer = Math.max(answer, count);
+            answer = Math.max(cnt, answer);
         }
         System.out.println(answer);
     }  
