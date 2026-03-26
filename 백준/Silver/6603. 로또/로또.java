7307dcaa20f09f1c037515dd33b09d2a9a246ab9
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-26 21:48:15 +0900

    [Silver II] Title: 로또, Time: 296 ms, Memory: 22160 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* dfs 조합 */ 
+class Main {
+    static int k;
+    static int[] arr;
+    static int[] select;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            k = Integer.parseInt(st.nextToken());
+            if(k == 0) break;
+            
+            arr = new int[k];
+            select = new int[6];
+            for(int i=0; i<k; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            dfs(0, 0);
+            System.out.println();
+        }
+    }
+    static void dfs(int start, int depth) {
+        if(depth == 6) {
+            for(int i=0; i<6; i++) {
+                System.out.print(select[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i=start; i<k; i++) {
+            select[depth] = arr[i];
+            dfs(i+1, depth+1);
+        }
+    }
+}
