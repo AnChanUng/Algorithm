13f59219a96f0ded9d5f4b14e0157ce4e659c1ed
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-26 16:12:20 +0900

    [level 2] Title: 소수 찾기, Time: 52.93 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -29,7 +29,7 @@ class Solution {
     
     static boolean isPrime(int num) {
         if(num <= 1) return true;
-        for(int i=2; i<num; i++) {
+        for(int i=2; i*i<=num; i++) {
             if(num % i == 0) {
                 return true;
             }
