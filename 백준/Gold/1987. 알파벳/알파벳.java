d8e7842926c06a0ccbe3aec6e5688e833144aa2a
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-26 20:53:39 +0900

    [Gold IV] Title: 알파벳, Time: 920 ms, Memory: 15380 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,12 +3,13 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    static int r, c;
-    static char[][] arr;
-    static boolean[] vis;
+    static int[][] arr;
+    static boolean[] alpha;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
     static int maxCnt;
+    static int cnt;
+    static int r, c;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,35 +17,36 @@ class Main {
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        arr = new char[r][c];
-        vis = new boolean[26];
-        
+        arr = new int[r][c];   
+        alpha = new boolean[26]; // 알파벳 지났는지 아닌지 
+
         for(int i=0; i<r; i++) {
             String str = br.readLine();
             for(int j=0; j<c; j++) {
                 arr[i][j] = str.charAt(j);
             }
         }
+
         maxCnt = 0;
-        vis[arr[0][0]-'A'] = true;
-        dfs(0, 0, 1);
-        System.out.println(maxCnt);
+        cnt = 1;
+        dfs(0, 0);
+        System.out.print(maxCnt);
     }
-    static void dfs(int x, int y, int cnt) {
+    static void dfs(int x, int y) {
+        //System.out.println("x: " + x + " y: "  + y + " cnt: " + cnt + " maxCnt: " + maxCnt);
         maxCnt = Math.max(maxCnt, cnt);
-        
+        alpha[arr[x][y]-'A'] = true;
         for(int dir=0; dir<4; dir++) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
             if(nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-
-            int next = arr[nx][ny] - 'A';
-
-            if(!vis[next]) {
-                vis[next] = true;
-                dfs(nx, ny, cnt+1);
-                vis[next] = false;
-            }
+            if(!alpha[arr[nx][ny]-'A']) {
+                cnt++;
+                alpha[arr[nx][ny]-'A'] = true;
+                dfs(nx, ny);
+                alpha[arr[nx][ny]-'A'] = false;
+                cnt--;
+            } 
         }
     }
 }
