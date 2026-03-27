ef8cc23c8cd878452f14a442bd065de33c711a61
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-27 19:34:59 +0900

    [level 2] Title: H-Index, Time: 0.92 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,15 +1,16 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
-        
         Arrays.sort(citations);
         
-        for(int i=0; i<citations.length; i++) {
-            if(citations[i] >= (citations.length - i)) {
-                return citations.length - i;
+        int len = citations.length;
+        for(int i=0; i<len; i++) {
+            int h = len - i;
+            if(citations[i] >= h) {
+                return h;
             }
         }
+        
         return 0;
     }
 }
