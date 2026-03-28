3267f0f275fa87da2695fc133f64e282beb9362e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-28 13:43:27 +0900

    [Gold IV] Title: 공유기 설치, Time: 260 ms, Memory: 29220 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -11,7 +11,6 @@ class Main {
         int c = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-        
         for(int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
@@ -19,20 +18,19 @@ class Main {
         Arrays.sort(arr);
 
         int left = 1;
-        int right = arr[n-1] - arr[0];
+        int right = arr[n-1]-arr[0];
         int answer = 0;
         while(left <= right) {
             int mid = (left + right) / 2;
             int last = arr[0];
             int cnt = 1;
-            
+
             for(int i=1; i<n; i++) {
                 if(arr[i] - last >= mid) {
                     cnt++;
                     last = arr[i];
                 }
             }
-
             if(cnt >= c) {
                 answer = mid;
                 left = mid + 1;
