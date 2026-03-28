1d3d40c10acfd759e08c7483815f1084621cd6e9
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-28 17:16:42 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -5,11 +5,11 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());  
         for(int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -21,15 +21,11 @@ class Main {
             int left = 0;
             int right = n-1;
             while(left < right) {
-                if(left == i) {
-                    left++;
-                    continue;
-                }
-                if(right == i) {
-                    right--;
-                    continue;
-                }
+                if(left == i) { left++; continue; }
+                if(right == i) { right--; continue; }
+                
                 int sum = arr[left] + arr[right];
+
                 if(sum < arr[i]) {
                     left++;
                 } else if (sum == arr[i]) {
