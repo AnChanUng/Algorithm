c93ba6ca168908263f496eba4fdb00937c1c52c4
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2026-03-29 16:09:09 +0900

    [level 2] Title: 피로도, Time: 3.32 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 87.4 MB, 시간: 4.77 ms
+메모리: 84.9 MB, 시간: 3.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 25일 17:16:21
+2026년 03월 29일 16:09:02
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,21 @@
 import java.util.*;
-
 class Solution {
-    static int maxCnt = 0;
     static boolean[] vis;
-    public int solution(int k, int[][] dungeons) {  
+    static int maxNum;
+    public int solution(int k, int[][] dungeons) {
         vis = new boolean[dungeons.length];
-        dfs(dungeons, k, 0, 0);   
-        return maxCnt;
+        dfs(dungeons, k, 0, 0);
+        return maxNum;
     }
     
-    static void dfs(int[][] arr, int sum, int cnt, int depth) {
-        //System.out.println("depth: " + depth + " cnt: " + cnt + " sum: " + sum + " vis: " + Arrays.toString(vis));
-        maxCnt = Math.max(cnt, maxCnt);
-        for(int i=0; i<arr.length; i++) {
-            if(!vis[i] && arr[i][0] <= sum) {
+    static void dfs(int[][] dungeons, int sum, int cnt, int depth) {
+        maxNum = Math.max(maxNum, cnt);
+        for(int i=0; i<dungeons.length; i++) {
+            if(!vis[i] && dungeons[i][0] <= sum) {
                 vis[i] = true;
-                dfs(arr, sum-arr[i][1], cnt+1, depth+1);
+                dfs(dungeons, sum-dungeons[i][1], cnt+1, depth+1);
                 vis[i] = false;
             }
-        }    
+        }
     }
 }
