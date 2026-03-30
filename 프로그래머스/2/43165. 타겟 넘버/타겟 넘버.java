bbc01c0ef59f7db534cf883bcf2538ce5c1540ac
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-03-31 00:02:31 +0900

    [level 2] Title: 타겟 넘버, Time: 6.53 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,18 @@
+import java.util.*;
 class Solution {
     static int answer = 0;
     public int solution(int[] numbers, int target) {
         dfs(numbers, 0, 0, target);
         return answer;
     }
-    static void dfs(int[] arr, int idx, int sum, int target) {
-        if(idx == arr.length) {
+    static void dfs(int[] arr, int depth, int sum, int target) {
+        if(depth == arr.length) {
             if(sum == target) {
                 answer++;
-            } 
+            }
             return;
         }
-        dfs(arr, idx+1, sum+arr[idx], target);
-        dfs(arr, idx+1, sum-arr[idx], target);
-        
+        dfs(arr, depth+1, sum+arr[depth], target); 
+        dfs(arr, depth+1, sum-arr[depth], target);  
     }
 }
