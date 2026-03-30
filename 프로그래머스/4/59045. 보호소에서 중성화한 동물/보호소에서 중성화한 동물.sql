211aeff477d3bcee8359345a7b741eaf3c8327af
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-03-30 14:28:12 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,8 +1,9 @@
-SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
+SELECT B.ANIMAL_ID, B.ANIMAL_TYPE, B.NAME
 FROM ANIMAL_INS A JOIN ANIMAL_OUTS B
 ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE A.SEX_UPON_INTAKE LIKE 'Intact %' 
-AND (B.SEX_UPON_OUTCOME IN('Neutered Male') OR B.SEX_UPON_OUTCOME IN('Spayed Female'))
-# WHERE (A.SEX_UPON_INTAKE = 'Intact Male' OR A.SEX_UPON_INTAKE = 'Intact Female')
-# AND (B.SEX_UPON_OUTCOME = 'Neutered Male' OR B.SEX_UPON_OUTCOME = 'Spayed Female')
-# 보호소에 들어올 당시에는 중성화 되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물
+-- 보호소에 들어올 당시에는 중성화되지 않았지만 
+-- 보호소를 나갈 당시에는 중성화된 동물
+WHERE A.SEX_UPON_INTAKE LIKE 'Intact %' AND
+(B.SEX_UPON_OUTCOME LIKE 'Neutered %' OR 
+B.SEX_UPON_OUTCOME LIKE 'Spayed %')
+ORDER BY B.ANIMAL_ID ASC
