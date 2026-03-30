f301ee86802673ec9f891af3ac1e5f0fcf98aa6b
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-03-30 14:03:48 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,6 @@
 SELECT A.NAME, A.DATETIME
 FROM ANIMAL_INS A LEFT JOIN ANIMAL_OUTS B
 ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE B.DATETIME IS NULL
+WHERE B.ANIMAL_ID IS NULL
 ORDER BY A.DATETIME ASC
-LIMIT 3
--- 아직 입양을 못 간 동물 중 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일 조회
--- 이때 결과는 보호 시작일 순으로 조회
+limit 3
