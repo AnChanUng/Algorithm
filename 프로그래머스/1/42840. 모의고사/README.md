815df72a6f9f2d125765017637a4db2c6741393e
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-09 17:58:53 +0900

    [level 1] Title: 모의고사, Time: 1.46 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.29 ms
+메모리: 74 MB, 시간: 1.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 24일 16:13:46
+2026년 04월 09일 17:58:51
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,44 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[] number1 = {1, 2, 3, 4, 5};
-        int[] number2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] number3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[][] arr = new int[3][2];
         
+        int[] f1 = {1, 2, 3, 4, 5};
+        int[] f2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] f3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        // 답 개수
         int cnt1 = 0;
         int cnt2 = 0;
         int cnt3 = 0;
         for(int i=0; i<answers.length; i++) {
-            if(number1[i%number1.length] == answers[i]) { cnt1++; }
-            if(number2[i%number2.length] == answers[i]) { cnt2++; }
-            if(number3[i%number3.length] == answers[i]) { cnt3++; }
+            if(answers[i] == f1[i%5]) cnt1++;
+            if(answers[i] == f2[i%8]) cnt2++;
+            if(answers[i] == f3[i%10]) cnt3++;
+        }     
+        // 1,2,3번 중 답 개수가 가장 큰 사람
+        arr[0][0] = cnt1;
+        arr[0][1] = 1;
+        arr[1][0] = cnt2;
+        arr[1][1] = 2;
+        arr[2][0] = cnt3;
+        arr[2][1] = 3;
+        
+        int[] res = new int[3];
+        Arrays.sort(arr, (a, b) -> Integer.compare(b[0], a[0]));
+        
+        for(int i=0; i<arr.length-1; i++) {
+            res[i] = arr[i][1];
+            if(arr[i][0] != arr[i+1][0]) {
+                break;
+            } else {
+                res[i+1] = arr[i+1][1];
+            }
         }
         
-        int maxCount = Math.max(cnt1, Math.max(cnt2, cnt3));
+        if (res[2] == 0 && res[1] == 0) return new int[]{res[0]};
+        if (res[2] == 0) return new int[]{res[0], res[1]};
         
-        List<Integer> result = new ArrayList<>();
-        if (cnt1 == maxCount) result.add(1);
-        if (cnt2 == maxCount) result.add(2);
-        if (cnt3 == maxCount) result.add(3);
-        
-        int[] resultArray = new int[result.size()];
-        for(int i=0; i<result.size(); i++) {
-            resultArray[i] = result.get(i);
-        }
-        
-        return resultArray;
+        return res;
     }
 }
