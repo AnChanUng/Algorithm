23753eceef3b36d9c2aac22e07318a160447dd0a
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-09 10:56:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 120.84 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 32.5 MB, 시간: 39.54 ms
+메모리: 97 MB, 시간: 120.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 03일 23:55:21
+2026년 04월 09일 10:56:31
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,24 +1,31 @@
 import java.util.*;
-
-class Solution {
+class Solution { // 전체선수들, 완주한 선수들
     public String solution(String[] participant, String[] completion) {
-        
+        String answer = "";
         Map<String, Integer> map = new HashMap<>();
-        
         for(int i=0; i<participant.length; i++) {
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
-        }
-        
-        for(int j=0; j<completion.length; j++) {
-            map.put(completion[j], map.get(completion[j])-1);
-        }
-        
-        for(String key : map.keySet()) {
-            if(map.get(key) > 0) {
-                return key;
+            if(!map.containsKey(participant[i])) {
+                map.put(participant[i], 1);
+            } else {
+                map.put(participant[i], map.get(participant[i]) + 1);
             }
         }
         
-        return "";
+        for(int i=0; i<completion.length; i++) {
+            // 사람이 없으면 answer에 저장
+            if(map.containsKey(completion[i])) {
+                map.put(completion[i], map.get(completion[i]) -1);
+            } else {
+                answer += completion[i];
+            }
+        }
+        
+        for(String x : map.keySet()) {
+            if(map.get(x) >= 1) {
+                answer += x;
+            } 
+        }
+        
+        return answer;
     }
 }
