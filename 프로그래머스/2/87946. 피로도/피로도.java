4b0882df67583e595c075b26b297e684bab2758c
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-09 21:02:37 +0900

    [level 2] Title: 피로도, Time: 4.68 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,19 +1,30 @@
 import java.util.*;
+/* 순열 백트래킹 dfs */
 class Solution {
     static boolean[] vis;
-    static int maxNum;
+    static int maxCnt;
     public int solution(int k, int[][] dungeons) {
         vis = new boolean[dungeons.length];
-        dfs(dungeons, k, 0, 0);
-        return maxNum;
+        maxCnt = 0;
+        
+        dfs(dungeons, k, 0);
+        
+        return maxCnt;
     }
     
-    static void dfs(int[][] dungeons, int sum, int cnt, int depth) {
-        maxNum = Math.max(maxNum, cnt);
+    static void dfs(int[][] dungeons, int k, int depth) {
+        int cnt = 0;
+        for(int i=0; i<vis.length; i++) {
+            if(vis[i]) {
+                cnt++;
+            }
+        }
+        maxCnt = Math.max(maxCnt, cnt);
+        
         for(int i=0; i<dungeons.length; i++) {
-            if(!vis[i] && dungeons[i][0] <= sum) {
+            if(!vis[i] && k >= dungeons[i][0]) {
                 vis[i] = true;
-                dfs(dungeons, sum-dungeons[i][1], cnt+1, depth+1);
+                dfs(dungeons, k - dungeons[i][1], depth+1);
                 vis[i] = false;
             }
         }
