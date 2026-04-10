0c91f5d3b9e0912adcc0ccf52ac0cef0470960b8
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-10 11:24:46 +0900

    [level 1] Title: K번째수, Time: 1.29 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,13 +1,15 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
+        // 1.commands[i][0] ~ commands[i][1] 자르기
+        // 2.오름차순 정렬 
+        // 3.commands[i][2] 번째 수 고르기
         for(int i=0; i<commands.length; i++) {
-            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(arr);
-            answer[i] = arr[commands[i][2]-1];
+            int[] res = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(res);
+            answer[i] = res[commands[i][2]-1];
         }
         
         return answer;
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 3.66 MB, 시간: 0.02 ms
+메모리: 94.9 MB, 시간: 1.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 16:44:20
+2026년 04월 10일 11:24:50
 
 ### 문제 설명
 
