192d1f1103e1063826d7e179293e25f13a845fff
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-14 22:23:42 +0900

    [Silver I] Title: 부등호, Time: 208 ms, Memory: 28528 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,52 +1,44 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
-/*
-  숫자: 1~9
-  부등호 만족
 
-  순열 백트래킹
-*/
 class Main {
-  static int[] num = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  static boolean[] vis;
-  static String[] oper;
-  static int k;
-  static String maxStr = null;
-  static String minStr = null;
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String[] oper;
+    static boolean[] vis;
+    static int k;
+    static String minNum = "";
+    static String maxNum = "";
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        k = Integer.parseInt(br.readLine());
 
-    k = Integer.parseInt(br.readLine());
-
-    oper = new String[k];
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    for(int i=0; i<k; i++) {
-      oper[i] = st.nextToken();
+        oper = new String[k];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<k; i++) {
+            oper[i] = st.nextToken();
+        }
+        vis = new boolean[10];
+        
+        dfs("", 0, 0);
+        System.out.println(maxNum);
+        System.out.println(minNum);
     }
-
-    vis = new boolean[num.length];
-    dfs("", 0, 0);
-    
-    System.out.println(maxStr);
-    System.out.println(minStr);
-  }
-  static void dfs(String str, int depth, int prev) {
-    if(depth == k+1) {
-      if(maxStr == null || str.compareTo(maxStr) > 0) maxStr = str;
-      if(minStr == null || str.compareTo(minStr) < 0) minStr = str;
-      return;
+    static void dfs(String str, int depth, int prev) {
+        if(depth == k+1) {
+            if(maxNum.equals("") || str.compareTo(maxNum) > 0) maxNum = str;
+            if(minNum.equals("") || str.compareTo(minNum) < 0) minNum = str;
+            return;
+        }
+        for(int i=0; i<10; i++) {
+            if(!vis[i]) {
+                if(depth > 0) {
+                    if(oper[depth-1].equals("<")) if(prev > i) continue;
+                    if(oper[depth-1].equals(">")) if(prev < i) continue;
+                }
+                vis[i] = true;
+                dfs(str + String.valueOf(i), depth+1, i);
+                vis[i] = false;
+            }
+        }
     }
-
-    for(int i=0; i<num.length; i++) {
-      if(!vis[i]) {
-        if(depth > 0) {
-          if(oper[depth-1].equals("<") && !(prev < num[i])) continue;
-          if(oper[depth-1].equals(">") && !(prev > num[i])) continue; 
-        } 
-        vis[i] = true;
-        dfs(str + num[i], depth+1, num[i]);
-        vis[i] = false;
-      }
-    }
-  }
 }
