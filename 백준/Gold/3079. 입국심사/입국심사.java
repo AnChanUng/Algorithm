abf97bf62da2cd03ce86f4dc794ebf1bd50aa801
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-21 14:18:59 +0900

    [Gold V] Title: 입국심사, Time: 288 ms, Memory: 23320 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -13,30 +13,29 @@ class Main {
     long maxTime = 0;
     for(int i=0; i<n; i++) {
       arr[i] = Integer.parseInt(br.readLine());
-      if(maxTime < arr[i]) {
-        maxTime = arr[i];
-      }
+      maxTime = Math.max(maxTime, arr[i]);
     }
 
     long low = 1;
     long high = (long) maxTime * m;
-    long ans = 0;
+    long maxAns = 0;
     while(low <= high) {
-      long mid = (high + low) / 2;
-
+      long mid = (low + high) / 2;
+      
       long answer = 0;
       for(int i=0; i<n; i++) {
         answer += mid / arr[i];
         if(answer >= m) break;
       }
-
+      
       if(answer >= m) {
-        ans = mid;
+        maxAns = mid;
         high = mid - 1;
       } else {
         low = mid + 1;
       }
     }
-    System.out.println(ans);
+
+    System.out.println(maxAns);
   }
 }
