046f810cea4980c3303335870d0476501e434643
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-24 13:55:53 +0900

    [Gold V] Title: 0 만들기, Time: 220 ms, Memory: 26156 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,63 +1,56 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
+/* 
+   1~n의 수에 +나 -를 삽입하고 결과가 0이 되는 경우의 수
 
-class Main {
+   알고리즘: dfs
+*/
+public class Main {
     static int t, n;
-    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
         t = Integer.parseInt(br.readLine());
 
         for(int test=0; test<t; test++) {
             n = Integer.parseInt(br.readLine());
-
-            dfs(2, "1");
-
-            sb.append("\n");
+            arr = new int[n];
+            dfs("1", 2, 1);
+            System.out.println();
         }
-        System.out.println(sb);
     }
-
-    static void dfs(int idx, String str) {
-        if(idx == n+1) {
-            if(calculate(str) == 0) {
-                sb.append(str).append("\n");
+    static void dfs(String str, int start, int depth) {
+        if(depth == n) {
+            if(calc(str) == 0) {
+                System.out.println(str);
             }
             return;
         }
-        dfs(idx+1, str + " " + idx);
-        dfs(idx+1, str + "+" + idx);
-        dfs(idx+1, str + "-" + idx);
+        for(int i=start; i<=n; i++) {
+            dfs(str + " " + i, i+1, depth+1);
+            dfs(str + "+" + i, i+1, depth+1);
+            dfs(str + "-" + i, i+1, depth+1);
+        }
     }
-
-    static int calculate(String str) {
-        str = str.replace(" ", ""); 
-        
+    static int calc(String str) {
         int sum = 0;
+        int sign = 1;
         int num = 0;
-        char op = '+';
         for(int i=0; i<str.length(); i++) {
             char ch = str.charAt(i);
-
             if(ch >= '0' && ch <= '9') {
-                num = num * 10 + (ch - '0'); 
-            } else {
-                if(op == '+') {
-                    sum += num;
-                } else {
-                    sum -= num;
-                }
-                op = ch;
+                num = num * 10 + (ch - '0');
+            } else if (ch == '+') {
+                sum += sign * num;
                 num = 0;
+                sign = 1;
+            } else if (ch == '-') {
+                sum += sign * num;
+                num = 0;
+                sign = -1;
             }
         }
-        if(op == '+') {
-            sum += num;
-        } else {
-            sum -= num;
-        }
-
+        sum += sign * num;
         return sum;
     }
 }
