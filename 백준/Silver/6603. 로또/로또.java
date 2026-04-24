5ddd2b3a5f7b5df5b7a57e74daa3ed0b7187ed7d
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-24 10:54:44 +0900

    [Silver II] Title: 로또, Time: 312 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,25 +1,28 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
+/* 
+   배열에서 6개를 골라 모든 경우의 수 출력
 
-class Main {
-    static int[] arr;
-    static int[] selected;
+   알고리즘: 조합 dfs
+*/
+public class Main {
     static int k;
+    static int[] arr;
+    static int[] res;
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        StringTokenizer st;
         while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             k = Integer.parseInt(st.nextToken());
-            
             if(k == 0) break;
-            arr = new int[k];
-            selected = new int[6];
             
+            arr = new int[k];
+            res = new int[k];
             for(int i=0; i<k; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
+
             dfs(0, 0);
             System.out.println();
         }
@@ -27,13 +30,13 @@ class Main {
     static void dfs(int start, int depth) {
         if(depth == 6) {
             for(int i=0; i<6; i++) {
-                System.out.print(selected[i] + " ");
+                System.out.print(res[i] + " ");
             }
             System.out.println();
             return;
         }
-        for(int i=start; i<arr.length; i++) {
-            selected[depth] = arr[i];
+        for(int i=start; i<k; i++) {
+            res[depth] = arr[i];
             dfs(i+1, depth+1);
         }
     }
