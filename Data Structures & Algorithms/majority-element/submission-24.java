9e852e1ee6dfc29b5edb017b8885cb6c5a2876a8
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-04-29 11:25:59 +0900

    Add: majority-element - submission-24

---
+++ Data Structures & Algorithms/majority-element/submission-24.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int maxCnt = 0;
+        int key = 0;
+        for(Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if(e.getValue() > maxCnt) {
+                maxCnt = e.getValue();
+                key = e.getKey();
+            }
+        }
+
+        return key;
+    }
+}
