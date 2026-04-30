4e9bee29c7251876876bb28c207164df454d2269
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-05-01 00:57:50 +0900

    [level 2] Title: 프로세스, Time: 1.70 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 4.43 MB, 시간: 0.02 ms
+메모리: 85.5 MB, 시간: 1.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 16일 02:09:53
+2026년 05월 01일 00:57:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,27 +1,31 @@
 import java.util.*;
-
 class Solution {
-    public int solution(int[] priorities, int location) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int p : priorities) {
-            pq.add(p);
+    public int solution(int[] priorities, int location) {        
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0; i<priorities.length; i++) {
+            q.add(new int[]{i, priorities[i]});
         }
         
-        while (!pq.isEmpty()) {
-            for(int i=0; i<priorities.length; i++) {
-                if(priorities[i] == pq.peek()) {
-                    pq.poll();
-                    answer++;
-                    
-                    if(i == location) {
-                        return answer;
-                    }
+        int rank = 1;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            boolean flag = false;
+            
+            for(int[] x : q) {
+                if(x[1] > cur[1]) {
+                    flag = true;
+                    break;
                 }
             }
+            if(flag) {
+                q.add(new int[]{cur[0], cur[1]});
+            } else {
+                if(cur[0] == location) {
+                    return rank;
+                }
+                rank++;
+            }
         }
-        
-        return answer;
+        return rank;
     }
 }
