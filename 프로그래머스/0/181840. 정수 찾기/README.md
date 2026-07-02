fab01760cac67a7883559ed6fa2156c7198220d2
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-07-02 20:53:20 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/README.md
+++ 프로그래머스/0/181840. 정수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정수 찾기 - 181840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181840?language=java) 
 
 ### 성능 요약
 
-메모리: 72.6 MB, 시간: 0.03 ms
+메모리: 72.9 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 15일 19:03:00
+2026년 07월 02일 20:52:57
 
 ### 문제 설명
 
--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int[] num_list, int n) {
+        int answer = 0;
         
-        for (int i=0; i<num_list.length; i++) {
+        for(int i=0; i<num_list.length; i++) {
             if(num_list[i] == n) {
-                return 1;
-            } else {
-                return 0;
+                answer = 1;
             }
         }
+        
+        return answer;
     }
 }
