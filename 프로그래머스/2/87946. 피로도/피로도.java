52e32083f1cde74ccedda20ec095a43eaed0b34b
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-05 14:43:32 +0900

    [level 2] Title: 피로도, Time: 1.87 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,31 +2,54 @@ import java.util.*;
 /* 순열 백트래킹 dfs */
 class Solution {
     static boolean[] vis;
+    static int[][] dungeons;
     static int maxCnt;
     public int solution(int k, int[][] dungeons) {
+        this.dungeons = dungeons;
         vis = new boolean[dungeons.length];
         maxCnt = 0;
         
-        dfs(dungeons, k, 0);
-        
+        dfs(k, 0);
         return maxCnt;
     }
     
-    static void dfs(int[][] dungeons, int k, int depth) {
-        int cnt = 0;
-        for(int i=0; i<vis.length; i++) {
-            if(vis[i]) {
-                cnt++;
-            }
-        }
-        maxCnt = Math.max(maxCnt, cnt);
+    static void dfs(int k, int depth) {
+        maxCnt = Math.max(maxCnt, depth);
         
         for(int i=0; i<dungeons.length; i++) {
-            if(!vis[i] && k >= dungeons[i][0]) {
+            if(!vis[i] && dungeons[i][0] <= k) {
                 vis[i] = true;
-                dfs(dungeons, k - dungeons[i][1], depth+1);
+                dfs(k - dungeons[i][1], depth+1);
                 vis[i] = false;
             }
         }
     }
 }
+
+// class Solution {
+//     public static boolean[] visited;
+//     //public static int answer;
+//     public static int maxNum;
+//     public int solution(int k, int[][] dungeons) {
+//         //int answer = 0;
+//         maxNum = 0;
+//         visited = new boolean[dungeons.length];
+//         generate(k,dungeons, 0);
+//         return maxNum;
+//     }
+//     public void generate(int k, int[][] dungeons, int depth) {
+//         maxNum = Math.max(maxNum, depth);
+        
+//         for(int i=0;i<dungeons.length;i++) {
+//             if(visited[i]==true) continue; 
+//             if(dungeons[i][0] > k) continue;
+            
+//             visited[i]=true;
+//             generate(k-=dungeons[i][1],dungeons, depth+1);
+//             //answer++;
+//             visited[i]=false;
+            
+//             //generate(k-=dungeons[i][1],dungeons);
+//         }
+//     }
+// }
