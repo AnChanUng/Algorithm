5c5a9553b4b54908cadd0ce4019cf26a9c303124
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-09 17:42:11 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2.70 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,24 +1,11 @@
 import java.util.*;
-/*
-    시작점에서 도착점까지 가는 최소 비용 계산c
-    :(0, 0) -> (n-1, n-1)
 
-    알고리즘: 다익스트라, 우선순위유
-
-    비어있는 칸: 0
-    벽: 1
-    직선도로 개수: 전체칸수에서 -1 (개당 100원)
-    코너: 방향을 튼 개수 (개당 500원)
-    
-    1. 0, 0에서 출발한다
-    2. 다익스트라
-*/
-class Solution {// 좌 상 우 하
+class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
     static int[][][] dist;
     static int n;
-    static int minTotal = Integer.MAX_VALUE;
+    static int minTotal;
     static class Node implements Comparable<Node> {
         int x;
         int y;
@@ -31,28 +18,27 @@ class Solution {// 좌 상 우 하
             this.cost = cost;
             this.direction = direction;
         }
-        // 가격 오름차순
+        
         public int compareTo(Node o) {
             return Integer.compare(this.cost, o.cost);
         }
     }
-    
     public int solution(int[][] board) {
+        minTotal = Integer.MAX_VALUE;
         n = board.length;
         
-        dijkstra(0, 0, board);
+        dijkstra(0, 0, board);        
         
         for(int i=0; i<4; i++) {
             minTotal = Math.min(minTotal, dist[n-1][n-1][i]);
         }
         
-        return minTotal - 500;
+        return minTotal;
     }
-    
     static void dijkstra(int x, int y, int[][] board) {
         dist = new int[n][n][4];
         for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+            for(int j=0; j<n; j++) { 
                 Arrays.fill(dist[i][j], Integer.MAX_VALUE);
             }
         }
@@ -69,15 +55,14 @@ class Solution {// 좌 상 우 하
                 if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
                 if(board[nx][ny] == 1) continue;
                 
-                // 현재 dir이랑 바뀌면 +500원
                 int nCost = cur.cost + 100;
-                if(cur.direction != dir) {
+                if(cur.direction != -1 && dir != cur.direction) {
                     nCost += 500;
                 }
                 
                 if(nCost < dist[nx][ny][dir]) {
-                    dist[nx][ny][dir] = nCost;
-                    pq.add(new Node(nx, ny, nCost, dir));
+                    dist[nx][ny][dir] = nCost; 
+                    pq.offer(new Node(nx, ny, nCost, dir));
                 }
             }
         }
