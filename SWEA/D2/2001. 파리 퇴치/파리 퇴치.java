a296e9cba556f9c1108f69e2cc62ea3210a26b3e
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-28 11:52:00 +0900

    [D2] Title: 파리 퇴치, Time: 79 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,45 +1,60 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 /*
-    1. n개의 배열 만들기
-    2. 배열에서 m*m의 합이 가장 큰 것 출력
-
-    알고리즘: 누적합
+    명령어를 수행했을 때, 수정된 결과의 처음 10개 숫자 출력
 */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/*
+ * N X N 배열에 숫자가 주어졌을 때, M * M의 숫자의 합이 가장 큰 구간 구하기
+ * 
+ * 1. 배열을 입력받는다.
+ * 2. M*M을 탐색하면서 현재 가장 큰수보다 크면 갱신한다.
+ * 3. 가장 큰 수를 출력한다.
+ */
 class Solution {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+	static int[][] arr;
+	static int maxSum = 0;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        
         int T = Integer.parseInt(br.readLine());
-
+        
         for(int test_case=1; test_case<=T; test_case++) {
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-
-            int[][] arr = new int[n][n];
+            
+            arr = new int[n][n];
+            
             for(int i=0; i<n; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                }
+            	st = new StringTokenizer(br.readLine());
+            	for(int j=0; j<n; j++) {
+            		arr[i][j] = Integer.parseInt(st.nextToken());
+            	}
             }
-
-            int maxSum = 0;
+            
+            maxSum = 0;
             for(int i=0; i<n-m+1; i++) {
-                for(int j=0; j<n-m+1; j++) {
-                    int sum = 0;
-                    for (int a=0; a<m; a++) {
-                        for(int b=0; b<m; b++) {
-                            sum += arr[i+a][j+b];
-                        }
-                    }
-                    maxSum = Math.max(maxSum, sum);
-                }
+            	for(int j=0; j<n-m+1; j++) {
+            		int sum = 0;
+            		for(int a=0; a<m; a++) {
+            			for(int b=0; b<m; b++) {
+            				sum += arr[i+a][j+b];
+            			}
+            		}
+            		if(maxSum < sum) {
+            			maxSum = sum;
+            		}
+            	}
             }
-            System.out.println("#" + test_case + " " + maxSum);
+            
+            sb.append("#").append(test_case).append(" ").append(maxSum).append("\n");
         }
+        System.out.print(sb);
     }
 }
