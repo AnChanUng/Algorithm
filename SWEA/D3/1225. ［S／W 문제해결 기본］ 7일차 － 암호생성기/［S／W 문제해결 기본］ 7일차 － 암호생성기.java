f246b0fae42d404af94abe77fd97b8acd531dc86
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-08-31 15:34:09 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 92 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,48 +1,51 @@
 import java.io.*;
 import java.util.*;
 /*
- * 1. 8개의 숫자 입력 받기
- * 2. 싸이클
- * 2.1 첫 번째 숫자를 1 감소한 뒤, 맨뒤로 보내기
- * 2.2 첫 번째 숫자를 2 감소한 뒤, 맨뒤로 보내기
- * 2.3 첫 번째 숫자를 3 감소한 뒤, 맨뒤로 보내기
- * 2.4 첫 번째 숫자가 0이 되었을 때, 맨뒤로 보내고 해당 배열 출력하기
- */
-public class Solution {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int T = 10;
-		
-		for(int test_case=1; test_case<=T; test_case++) {
-			int tc = Integer.parseInt(br.readLine());
-			
-			Queue<Integer> q = new LinkedList<>();
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i=0; i<8; i++) {
-				int x = Integer.parseInt(st.nextToken());
-				q.add(x);
-			}
-			
-			int num = 1;
-			while(true) {
-				int cur = q.poll();
-				if(cur - num <= 0) {
-					q.add(0);
-					break;
-				} else {
-					q.add(cur - num);
-				}
-				num++;
-				if(num >= 6) num = 1;
-				//System.out.println("num: " + num + " cur: " + cur);
-			}
-			
-			System.out.println("#" + test_case + " ");
-			for(int x : q) {
-				System.out.print(x + " ");
-			}
-		}
-	}
+    알고리즘: 시뮬레이션
+	
+	1. 8개의 숫자를 입력받는다
+	2.1 첫번째 수를 1 감소한 뒤, 맨 뒤로 보낸다.
+	2.2 첫번째 수를 2,3,4,5 감소한 뒤, 맨 뒤로 보낸다.
+	2.3 첫번째 수가 0보다 작아지는 경우 맨 뒤로 보내고 프로그램을 종료한다.
+	3. 현재 큐를 출력한다.
+*/
+class Solution {
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	StringTokenizer st;
+
+    	for(int test_case=1; test_case<=10; test_case++) {
+    		Deque<Integer> q = new ArrayDeque<>();
+    		
+    		int t = Integer.parseInt(br.readLine());
+    		st = new StringTokenizer(br.readLine());
+    		for(int i=0; i<8; i++) {
+    			q.offer(Integer.parseInt(st.nextToken()));
+    		}
+    		
+    		int cnt = 1;
+    		while(true) {
+    			int cur = q.poll();
+
+    			if(cur - cnt <= 0) {
+    				q.offer(0);
+    				break;
+    			} else {
+    				q.offer(cur - cnt);
+    			}
+    			
+    			cnt++;
+    			if(cnt >= 6) cnt = 1;
+    		}
+
+    		sb.append("#").append(test_case).append(" ");
+    		while(!q.isEmpty()) {
+    			int cur = q.poll();
+    			sb.append(cur).append(" ");
+    		}
+    		sb.append("\n");
+    	}
+    	System.out.print(sb);
+    }
 }
