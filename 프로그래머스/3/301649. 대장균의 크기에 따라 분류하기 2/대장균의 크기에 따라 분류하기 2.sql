4baa3eb24352ee0e8b55a11e9e93f773f70e0b4f
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-11 00:18:29 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,13 +1,25 @@
--- 코드를 작성해주세요
-SELECT ID, 
+/*
+    대중균 개체 크기 내림차순 정렬
+    개체의 크기가 를 아래와 같이 분류
+     상위 0 ~ 25% CRITICAL ->  ID가 ID의 개수 x 0.25
+     상위 26 ~ 50% HIGH ->  ID가 ID의 개수 x 0.50
+     상위 51 ~ 75 MEDIUM ->  ID가 ID의 개수 x 0.75
+     상위 76 ~ 100 LOW -.  ID가 ID의 개수 x 1
+     대장균 개체의 ID와 분류된 이름을 출력
+    개체 ID에 대해 오름차순
+*/
+SELECT 
+    ID,
     CASE 
-        WHEN SIZE = 1 THEN 'CRITICAL'
-        WHEN SIZE = 2 THEN 'HIGH'
-        WHEN SIZE = 3 THEN 'MEDIUM'
+        WHEN RNUM <= TOTAL * 0.25 THEN 'CRITICAL'
+        WHEN RNUM <= TOTAL * 0.50 THEN 'HIGH'
+        WHEN RNUM <= TOTAL * 0.75 THEN 'MEDIUM'
         ELSE 'LOW'
     END AS COLONY_NAME
-FROM (
-    SELECT ID, NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS SIZE
+FROM ( 
+    SELECT 
+        ID, 
+        ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) AS RNUM, COUNT(*) OVER () AS TOTAL
     FROM ECOLI_DATA
-) AS t
+) AS T
 ORDER BY ID ASC
