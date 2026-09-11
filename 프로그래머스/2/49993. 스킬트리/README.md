21f3c5a8ff292c7f1375cb1b1958da499d03f9cc
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-11 13:45:56 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 0.05 ms
+메모리: 74.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 07일 14:27:12
+2026년 09월 11일 13:45:51
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,35 +1,48 @@
 import java.util.*;
-
+/*
+    스킬 순서대로 배우기 순서대로 동작하면 cnt++
+    
+    1. skill_trees 배열 순회
+    2. 항상 skill의 첫번째 값부터 시작해야함
+    2.1 첫번째 값이 나오면 skill의 두번째 값이 나와야함 
+    2.2 첫번째 값이 안나오면 return false;
+    2.3 마지막 값 까지 돌면 cnt++
+    3. cnt 갯수 출력
+*/
 class Solution {
-    static HashMap<Character, Integer> map;
-    static int answer;
     public int solution(String skill, String[] skill_trees) {
-        answer = 0;
-        map = new HashMap<>();
+        int cnt = 0;
         
+        HashMap<Character, Integer> map = new HashMap<>();
         for(int i=0; i<skill.length(); i++) {
             char ch = skill.charAt(i);
-            map.put(ch, i);
+            if(!map.containsKey(ch)) {
+                map.put(ch, i);
+            }
         }
-       
+        
         for(int i=0; i<skill_trees.length; i++) {
-            int curIdx = -1;
-            boolean possible = true;
+            int index = 0;
+            boolean flag = false;
             for(int j=0; j<skill_trees[i].length(); j++) {
                 char ch = skill_trees[i].charAt(j);
 
-                if(!map.containsKey(ch)) { continue; }
-                
-                int chIdx = map.get(ch);
-                
-                if(curIdx + 1 != chIdx) {
-                    possible = false;
-                    break;
+                // 2.1 첫번째 값이 나오면 skill의 두번째 값이 나와야함 
+                // 2.2 첫번째 값이 안나오면 return false;
+                // 2.3 마지막 값 까지 돌면 cnt++
+                //System.out.println("ch: " + ch + " map.get(ch): " + map.get(ch));
+                if(map.containsKey(ch)) {
+                    if(map.get(ch) == index) {
+                        index++;
+                    } else {
+                        flag = true;
+                    }
                 }
-                curIdx = chIdx;
+                if(flag) break;
             }
-            if(possible) answer++;
-        }
-        return answer;
+            if(!flag) cnt++;
+        } 
+        
+        return cnt;
     }
 }
