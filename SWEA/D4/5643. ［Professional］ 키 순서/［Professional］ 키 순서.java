1da9c1ed141d996d5fa4e2c0230a2335012f0214
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-14 21:53:43 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,841 ms, Memory: 103,328 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,72 +1,68 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 /*
-    *자신의 키가 몇 번째 인지 알 수 있는 학생이 모두 몇명인지
+ * 인접리스트 DFS
+ */
+public class Solution {
+	static List<Integer>[] up;
+	static List<Integer>[] down;
+	static boolean[] vis;
+	static int cnt;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int test_case=1; test_case<=t; test_case++) {
+			int n = Integer.parseInt(br.readLine());
+			int m = Integer.parseInt(br.readLine());
+			
+			up = new List[n+1];
+			down = new List[n+1];
+			for(int i=1; i<=n; i++) {
+				up[i] = new ArrayList<>();
+				down[i] = new ArrayList<>();
+			}
+			
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				up[a].add(b);
+				down[b].add(a);
+			}
+			
+			int total = 0;
+			int answer = 0;
+			for(int i=1; i<=n; i++) {
+				vis = new boolean[n+1];
+				cnt = 0;
+				dfs(i, up);
+				total = cnt;
 
-    *알고리즘: DFS, 인접리스트
-*/
-class Solution {
-    static List<Integer>[] up;
-    static List<Integer>[] down;
-    static boolean[] vis;
-    static int n, m;
-    static int cnt;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-
-        for(int tc=1; tc<=t; tc++) {
-            n = Integer.parseInt(br.readLine()); // 학생 수
-            m = Integer.parseInt(br.readLine()); // 학생 키 비교 횟수
-
-            up = new List[n+1];
-            down = new List[n+1];
-            for(int i=1; i<=n; i++) {
-                up[i] = new ArrayList<>();
-                down[i] = new ArrayList<>();
-            }
-            
-            for(int i=0; i<m; i++) {
-                st = new StringTokenizer(br.readLine());
-                
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                up[a].add(b);
-                down[b].add(a);
-            }
-        
-            int answer = 0;
-            int total;
-            
-            for(int i=1; i<=n; i++) {
-                vis = new boolean[n+1];
-                cnt = 0;
-                vis[i] = true;
-                dfs(i, up);
-                total = cnt;
-                
-                vis = new boolean[n+1];
-                cnt = 0;
-                vis[i] = true;
-                dfs(i, down);
-                total += cnt;
-                
-                if(total == n-1) answer++;
-            }
-            sb.append("#").append(tc).append(" ").append(answer).append("\n");
-        }
-        System.out.println(sb.toString());
-    }
-    static void dfs(int node, List<Integer>[] graph) {
-        for(int next : graph[node]) {
-            if(!vis[next]) {
-                vis[next] = true;
-                cnt++;
-                dfs(next, graph);
-            }
-        }
-    }
+				vis = new boolean[n+1];
+				cnt = 0;
+				dfs(i, down);
+				total += cnt;
+				
+				if(total == n-1) {
+					answer++;
+				}
+			}
+			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+	static void dfs(int node, List<Integer>[] graph) {
+		vis[node] = true;
+		for(int next : graph[node]) {
+			if(!vis[next]) {
+				vis[next] = true;
+				cnt++;
+				dfs(next, graph);
+			}
+		}
+	}
 }
