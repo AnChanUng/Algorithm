0402710261ceef0e9680528d788911fafeee2de0
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-17 23:01:00 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 98 ms, Memory: 28,544 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,20 +1,13 @@
+
 import java.io.*;
 import java.util.*;
 /*
- * n명의 사람이 1~N번호를 붙이고 있다.
- * 서로 아는 사람끼리를 나타내는 번호 2개가 주어지는데
- * 건너건너 아는사람끼리 무리라고 했을 때, 몇 무리인지
- * 
- * 알고리즘: 그래프 dfs, 인접리스트
- * 
- * 각 주어진 숫자들을 통해 인접리스트를 만든다
- * dfs(int node)
- * - dfs를 1번부터 돌면서 호출되는 횟수를 구한다
+ * 모두를 혼자인 무리로 초기화
+ * 간선을 읽을 때마다 무리를 합침
+ * 루트인 노드를 세기 (=무리의 개수)
  */
 public class Solution {	
-	static List<Integer>[] graph;
-	static boolean[] vis;
-	static int n, m;
+	static int[] parent;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -24,27 +17,24 @@ public class Solution {
 		
 		for(int test_case=1; test_case<=t; test_case++) {
 			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
 			
-			graph = new List[n+1];
-			vis = new boolean[n+1];
+			parent = new int[n+1];
 			for(int i=1; i<=n; i++) {
-				graph[i] = new ArrayList<>();
+				parent[i] = i;
 			}
 			
 			for(int i=0; i<m; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				graph[a].add(b);
-				graph[b].add(a);
+				union(a, b);
 			}
 			
 			int cnt = 0;
 			for(int i=1; i<=n; i++) {
-				if(!vis[i]) {
-					dfs(i);
+				if(find(i) == i) {
 					cnt++;
 				}
 			}
@@ -53,12 +43,17 @@ public class Solution {
 		}
 		System.out.print(sb);
 	}
-	private static void dfs(int node) {
-		vis[node] = true;
-		for(int next : graph[node]) {
-			if(!vis[next]) {
-				dfs(next);
-			}
-		}
+	
+	private static int find(int x) {
+		if(parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
+	}
+	
+	private static boolean union(int a, int b) {
+		a = find(a);                                      
+		b = find(b);
+		if(a == b) return false;     
+		parent[b] = a;                                     
+		return true;
 	}
 }
