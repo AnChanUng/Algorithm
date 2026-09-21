4385b49a3f59d420425887f007d0ce238188b88c
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-21 22:18:49 +0900

    [level 3] Title: 입국심사, Time: 20.48 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,31 +1,53 @@
 import java.util.*;
-
+/*
+    알고리즘: 파라메트릭 서치
+    탐색 대상: 모든 사람이 심사를 받는 시간 T
+    범위: left = ? 
+         right = ?
+    
+    판정: n분 동안 처리 인원 = Σ (T / time[i]) 가 n 이상인가?
+    
+    - 가능하면 → 답 후보로 저장, 더 짧은 시간 탐색
+    - 불가능하면 → 더 긴 시간 탐색
+*/
 class Solution {
+    static int[] time;
     public long solution(int n, int[] times) {
+        long answer = 0;
+        time = times;                           
         
-        Arrays.sort(times);
+        long maxTime = 0;
+        for (int i = 0; i < time.length; i++) {
+            maxTime = Math.max(maxTime, time[i]);
+        }
         
-        long time = 1;
-        long high = (long) times[times.length-1] * n;
-        long low = 0;
-        long sum;
+        long left = 1;               
+        long right = maxTime * n;
         
-        while(low <= high) {
-            long mid = (low + high) / 2;
-            //System.out.println("mid: " + mid);
-            sum = 0;
+        while(left <= right) {
+            long mid = (left + right) / 2;
             
-            for(int x : times) {
-                sum += mid / x;
-            }
-            
-            if(sum >= n) {
-                high = mid - 1;
-                time = mid;
+            if(isValid(mid, n)) {
+                answer = mid;
+                right = mid - 1;
             } else {
-                low = mid + 1;
+                left = mid + 1;
             }
         }
-        return time;
+        
+        return answer;
+    }
+    
+    private static boolean isValid(long t, int n) {
+        long count = 0;
+        
+        for(int i=0; i<time.length; i++) {
+            count += t / time[i];
+            if(count >= n) { 
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
