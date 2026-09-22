88cdc9bd1bc00594c7e4fcdd8f92d21138a8688b
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-22 22:59:24 +0900

    [level 2] Title: 호텔 대실, Time: 10.70 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,50 +1,31 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] book_time) {
-        int room = 0;
-        
-        // 각 문자를 정수로 변경하고 배열에 저장
         int[][] bookTime = new int[book_time.length][2];
         for(int i=0; i<book_time.length; i++) {
-            String[] a = book_time[i][0].split(":");
-            String[] b = book_time[i][1].split(":");
-            
-            bookTime[i][0] = Integer.parseInt(a[0]) * 60 + Integer.parseInt(a[1]);
-            bookTime[i][1] = Integer.parseInt(b[0]) * 60 + Integer.parseInt(b[1]);
+            bookTime[i][0] = toMinute(book_time[i][0]);
+            bookTime[i][1] = toMinute(book_time[i][1]) + 10;
         }
         
         Arrays.sort(bookTime, (a, b) -> Integer.compare(a[0], b[0]));
         
-        System.out.println(Arrays.deepToString(bookTime));
-        
-        int idx = 0;
-        int curTime = -1;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        // bookTime 순회
+        int answer = 0;
         for(int i=0; i<bookTime.length; i++) {
-            // 처음이면 방을 생성
-            // pq에 퇴실시간 저장
-            if(i == 0) {
-                pq.offer(bookTime[i][1]);
-                continue;
+            while(!pq.isEmpty() && pq.peek() <= bookTime[i][0]) {
+                pq.poll();
             }
-
-            // 입실시간이 pq에 있는 퇴실시간+10보다 크면
-            // 방생성
-            // 현재 pq에 퇴실시간 저장
-            int cur = pq.poll();
-            if(bookTime[i][0] < cur + 10) {
-                pq.offer(bookTime[i][1]);
-                pq.offer(cur);
-            } 
-            
-            // 입실시간이 pq에 있는 퇴실시간+10보다 작거나 같으면
-            // 현재 pq에 있는걸 뺴주고 퇴실시간 저장
-            else {
-                pq.offer(bookTime[i][1]);    
-            }
+            pq.offer(bookTime[i][1]);
+            answer = Math.max(answer, pq.size());
         }
-        return pq.size();
+        
+        return answer;
+    }
+    
+    private static int toMinute(String time) {
+        String[] hm = time.split(":");
+        return Integer.parseInt(hm[0]) * 60 + Integer.parseInt(hm[1]);
     }
 }
