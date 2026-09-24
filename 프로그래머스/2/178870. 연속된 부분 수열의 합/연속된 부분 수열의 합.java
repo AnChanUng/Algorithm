e1b988c994fdbfec9ae6732a5229154defad985a
Chanung An <104750924+AnChanUng@users.noreply.github.com>
2026-09-24 17:24:30 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 7.14 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,40 +1,42 @@
 import java.util.*;
-import java.io.*;
 /*
-    부분 수열의 합이 k이고 길이가 짧은 수열 구하기
+    연속된 수의 합이 k중에 가장 짧은 것
     
     알고리즘: 슬라이딩 윈도우
-
-    배열 인덱스를 늘려가며 값을 더해주고
-    값이 k가 될때 break
+    
+    sequence 배열 순회
+    1. sum += sequence[i]를 더함
+    2. sum이 k보다 크면 
+    2.1 left++, sum -= sequence[i];
+    3. sum == k이면
+    3.1 left++
+    3.2 left, right 인덱스 저장
 */
 class Solution {
-    static int[] result;
     public int[] solution(int[] sequence, int k) {
-        result = new int[2];
+        int[] answer = new int[2];
         
-        int sum = 0;
         int left = 0;
+        int sum = 0;
         int minLen = Integer.MAX_VALUE;
-        for(int right=0; right<sequence.length; right++) {
+        for(int right = 0; right < sequence.length; right++) {
             sum += sequence[right];
             
             while(sum > k) {
                 sum -= sequence[left];
                 left++;
             }
-            
+        
             if(sum == k) {
                 int len = right - left;
-                
                 if(len < minLen) {
+                    answer[0] = left;
+                    answer[1] = right;
                     minLen = len;
-                    result[0] = left;
-                    result[1] = right;
                 }
             }
         }
         
-        return result;
+        return answer;
     }
 }
